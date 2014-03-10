c13be4d2746f37d0ca5202ea19cddb4fdb409fcd
Joshua McFarland <mcfarljw@gmail.com>
2014-03-10 16:48:00 -0400

    Add overflow-x to non-cordova study view

--- public_html/js/app/views/Study.js
+++ public_html/js/app/views/Study.js
@@ -33,6 +33,8 @@ define([
          */
         render: function() {
             this.$el.html(templateStudy);
+            if (!window.cordova)
+                this.$('.view').css("overflow-x", "hidden");
             skritter.user.scheduler.filter({ids: ['mcfarljwtest2-zh-幼儿-1-rune']});
             //skritter.user.scheduler.filter({parts: ['rune']});
             if (this.review) {
