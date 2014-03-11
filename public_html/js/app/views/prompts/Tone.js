57db488e106a17dca8c0063f22be7d427251d227
Joshua McFarland <mcfarljw@gmail.com>
2014-03-11 10:02:14 -0400

    Hooked back in tone recognition

--- public_html/js/app/views/prompts/Tone.js
+++ public_html/js/app/views/prompts/Tone.js
@@ -21,6 +21,7 @@ define([
         initialize: function() {
             Prompt.prototype.initialize.call(this);
             Tone.canvas = new Canvas();
+            this.listenTo(Tone.canvas, 'input:up', this.handleInput);
         },
         /**
          * @method render
@@ -30,6 +31,32 @@ define([
             this.$el.html(templateTone);
             Tone.canvas.setElement(this.$('#canvas-container'));
             Prompt.prototype.render.call(this);
+            return this;
+        },
+        /**
+         * @method clear
+         * @returns {Backbone.Model}
+         */
+        clear: function() {
+            Tone.canvas.render();
+            Prompt.prototype.clear.call(this);
+        },
+        /**
+         * @method handleInput
+         * @param {Array} points
+         * @param {CreateJS.Shape} shape
+         */
+        handleInput: function(points, shape) {
+            var result = Prompt.review.character().recognize(points, shape);
+            if (result) {
+                Tone.canvas.tweenShape('display', result.userShape(), result.inflateShape());
+                if (Prompt.review.character().isFinished()) {
+                    Tone.canvas.disableInput();
+                    Tone.canvas.injectLayerColor('display', 'green');
+                    Prompt.gradingButtons.show();
+                    this.showReading(Prompt.review.get('position') + 1);
+                }
+            }
         },
         /**
          * @method resize
