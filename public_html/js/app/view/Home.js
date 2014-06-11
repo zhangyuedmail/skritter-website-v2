b3b02f78448375c49dd915ee714f6b5512933aa4
Joshua McFarland <mcfarljw@gmail.com>
2014-06-11 08:41:50 -0700

    Updated home view stats example a bit

--- public_html/js/app/view/Home.js
+++ public_html/js/app/view/Home.js
@@ -21,6 +21,11 @@ define([
             this.$el.html(_.template(template, skritter.strings));
             BaseView.prototype.render.call(this).renderElements();
             this.elements.dueCount.text(skritter.user.scheduler.getDueCount(true));
+            if (skritter.user.data.reviews.length > 1) {
+                this.elements.buttonSync.show();
+            } else {
+                this.elements.buttonSync.hide();
+            }
             return this;
         },
         /**
@@ -30,13 +35,22 @@ define([
             BaseView.prototype.renderElements.call(this);
             this.elements.dueCount = this.$('.due-count');
             this.elements.listCount = this.$('.list-count');
+            this.elements.buttonSync = this.$('.button-sync');
         },
         /**
          * @property {Object} events
          */
         events: function() {
             return _.extend({}, BaseView.prototype.events, {
+                'vclick .button-sync': 'handleSyncClicked'
             });
+        },
+        /**
+         * @method handleSyncClicked
+         * @param {Object} event
+         */
+        handleSyncClicked: function(event) {
+            event.preventDefault();
         }
     });
     
