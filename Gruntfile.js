3e42620d4f04109394e919a49db3d9c3f97c80e0
Joshua McFarland <mcfarljw@gmail.com>
2014-06-20 10:26:45 -0700

    Support config replace in both root and www

--- Gruntfile.js
+++ Gruntfile.js
@@ -244,6 +244,7 @@ module.exports = function(grunt) {
                     }
                 },
                 files: [
+                    {src: 'config.xml', dest: 'build/cordova/chinese', expand: true, cwd: 'build/cordova/chinese'},
                     {src: 'config.xml', dest: 'build/cordova/chinese/www', expand: true, cwd: 'build/cordova/chinese/www'},
                     {src: 'main.js', dest: 'build/cordova/chinese/www/js', expand: true, cwd: 'build/cordova/chinese/www/js'},
                     {src: 'InAppBillingPlugin.java', dest: 'build/cordova/chinese/plugins/com.jernung.cordova.inappbilling/inappbilling', expand: true, cwd: 'build/cordova/chinese/plugins/com.jernung.cordova.inappbilling/inappbilling'}
@@ -261,6 +262,7 @@ module.exports = function(grunt) {
                     }
                 },
                 files: [
+                    {src: 'config.xml', dest: 'build/cordova/japanese', expand: true, cwd: 'build/cordova/japanese'},
                     {src: 'config.xml', dest: 'build/cordova/japanese/www', expand: true, cwd: 'build/cordova/japanese/www'},
                     {src: 'main.js', dest: 'build/cordova/japanese/www/js', expand: true, cwd: 'build/cordova/japanese/www/js'},
                     {src: 'InAppBillingPlugin.java', dest: 'build/cordova/japanese/plugins/com.jernung.cordova.inappbilling/inappbilling', expand: true, cwd: 'build/cordova/japanese/plugins/com.jernung.cordova.inappbilling/inappbilling'}
