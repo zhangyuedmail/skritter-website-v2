359a1a15d25dade0e3ffc80dd28314235e674404
Joshua McFarland <mcfarljw@gmail.com>
2014-07-03 15:52:20 -0700

    Clean up base code for new async scheduling

---
+++ public_html/js/app/model/user/Scheduler.js
@@ -0,0 +1,207 @@
+define([], function() {
+    /**
+     * @class UserScheduler
+     */
+    var UserScheduler = Backbone.Model.extend({
+        /**
+         * @method initialize
+         */
+        initialize: function() {
+            this.running = false;
+            this.worker = null;
+            if (Modernizr.webworkers) {
+                this.worker = new Worker('js/app/worker/SortSchedule.js');
+                this.worker.addEventListener('message', _.bind(this.handleWorkerFinished, this), false);
+            }
+        },
+        /**
+         * @property {Object} defaults
+         */
+        defaults: {
+            data: []
+        },
+        /**
+         * @method clear
+         * @returns {UserScheduler}
+         */
+        clear: function() {
+            this.set('data', []);
+            return this;
+        },
+        /**
+         * @method getDue
+         * @returns {Array}
+         */
+        getDue: function() {
+            return this.get('data').filter(function(item) {
+                return item.readiness >= 1.0;
+            });
+        },
+        /**
+         * @method getDueCount
+         * @returns {Number}
+         */
+        getDueCount: function() {
+            return this.getDue().length;
+        },
+        /**
+         * @method getNext
+         * @param {Function}callback
+         */
+        getNext: function(callback) {
+            var data = this.get('data');
+            var position = 0;
+            function next() {
+                var item = data[position];
+                if (!item) {
+                    return null;
+                }
+                skritter.user.data.loadItem(item.id, function(item) {
+                    if (item) {
+                        callback(item);
+                    } else {
+                        position++;
+                        next();
+                    }
+                });
+            }
+            next();
+        },
+        /**
+         * @method handleWorkerFinished
+         * @param {Object} event
+         */
+        handleWorkerFinished: function(event) {
+            var data = event.data;
+            this.set('data', data);
+            this.running = false;
+            this.trigger('sorted', data);
+            event.preventDefault();
+        },
+        /**
+         * @method update
+         * @param {Array|Object} items
+         * @returns {UserScheduler}
+         */
+        insert: function(items) {
+            items = Array.isArray(items) ? items : [items];
+            var data = this.get('data');
+            for (var i = 0, length = items.length; i < length; i++) {
+                var item = items[i];
+                var itemPosition = _.findIndex(data, {id: item.id});
+                if (item.vocabIds.length === 0) {
+                    continue;
+                } else if (itemPosition === -1) {
+                    data.push({
+                        id: item.id,
+                        last: item.last ? item.last : 0,
+                        next: item.next ? item.next : 0,
+                        part: item.part,
+                        style: item.style
+                    });
+                } else {
+                    data[itemPosition] = {
+                        id: item.id,
+                        last: item.last ? item.last : 0,
+                        next: item.next ? item.next : 0,
+                        part: item.part,
+                        style: item.style
+                    };
+                }
+            }
+            this.set('data', data);
+            return this;
+        },
+        /**
+         * @method loadAll
+         * @param {Function} callback
+         */
+        loadAll: function(callback) {
+            skritter.storage.getSchedule(_.bind(function(data) {
+                this.set('data', data);
+                this.trigger('loaded', data);
+                this.sort();
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            }, this));
+        },
+        /**
+         * @method remove
+         * @param {Array|Object} items
+         * @returns {UserScheduler}
+         */
+        remove: function(items) {
+            items = Array.isArray(items) ? items : [items];
+            var data = this.get('data');
+            for (var i = 0, length = items.length; i < length; i++) {
+                var item = items[i];
+                var itemPosition = _.findIndex(this.data, {id: item.id});
+                data.splice(itemPosition, 1);
+            }
+            this.set('data', data);
+            return this;
+        },
+        /**
+         * @method sort
+         */
+        sort: function() {
+            console.log('scheduler: sorting');
+            this.running = true;
+            if (this.worker) {
+                this.sortAsync();
+            } else {
+                this.sortSync();
+            }
+        },
+        /**
+         * @method sortAsync
+         */
+        sortAsync: function() {
+            this.worker.postMessage({
+                activeParts: skritter.user.getActiveParts(),
+                activeStyles: skritter.user.getActiveStyles(),
+                data: this.get('data')
+            });
+        },
+        /**
+         * @method sortSync
+         */
+        sortSync: function() {
+            var activeParts = skritter.user.getActiveParts();
+            var activeStyles = skritter.user.getActiveStyles();
+            var data = this.get('data').sort(function(item) {
+                var seenAgo = now - item.last;
+                var rtd = item.next - item.last;
+                var readiness = seenAgo / rtd;
+                //filter out inactive parts and styles
+                if (activeParts.indexOf(item.part) === -1 ||
+                    activeStyles.indexOf(item.style) === -1) {
+                    item.readiness = 0;
+                    return -item.readiness;
+                }
+                //randomly deprioritize new spaced items
+                if (!item.last && item.next - now > 600) {
+                    item.readiness = skritter.fn.randomDecimal(0.1, 0.3);
+                    return -item.readiness;
+                }
+                //randomly prioritize new items
+                if (!item.last || item.next - item.last === 1) {
+                    item.readiness = skritter.fn.randomInterval(9999);
+                    return -item.readiness;
+                }
+                //deprioritize overdue items
+                if (readiness > 9999) {
+                    item.readiness = skritter.fn.randomInterval(9999);
+                    return -item.readiness;
+                }
+                item.readiness = readiness;
+                return -item.readiness;
+            });
+            this.running = false;
+            this.trigger('sorted', data);
+        }
+    });
+
+    return UserScheduler;
+});
--- public_html/js/app/worker/SortSchedule.js
+++ public_html/js/app/worker/SortSchedule.js
@@ -1,27 +1,21 @@
 self.addEventListener('message', function(event) {
     //declarations
-    var data = event.data.schedule;
     var activeParts = event.data.activeParts;
     var activeStyles = event.data.activeStyles;
-    var now = event.data.now;
-    var spacedItems = event.data.spacedItems;
+    var data = event.data.data;
+    var now = getUnixTime();
     //functions
-    function findIndex(objectArray, id) {
-        for (var i = 0, length = objectArray.length; i < length; i++) {
-            if (objectArray[i].id === id) {
-                return i;
-            }
-        }
-        return -1;
-    }
+    function getUnixTime() {
+        return Math.round(new Date().getTime() / 1000);
+    };
     function randomDecimal(min, max) {
         return Math.random() * (max - min) + min;
     }
     function randomInterval(value) {
         return Math.round(value * (0.925 + (Math.random() * 0.15)));
     }
-    //process
-    data = data.sort(function(item) {
+    //actions
+    data.sort(function(item) {
         var seenAgo = now - item.last;
         var rtd = item.next - item.last;
         var readiness = seenAgo / rtd;
@@ -30,17 +24,6 @@ self.addEventListener('message', function(event) {
             activeStyles.indexOf(item.style) === -1) {
             item.readiness = 0;
             return -item.readiness;
-        }
-        //deprioritize items currently being spaced
-        var spacedItemIndex = findIndex(spacedItems, item.id);
-         if (spacedItemIndex !== -1) {
-            var spacedItem = spacedItems[spacedItemIndex];
-            if (spacedItem.until > now) {
-                item.readiness = randomDecimal(0.1, 0.3);
-                return -item.readiness;
-            } else {
-                spacedItems.splice(spacedItemIndex, 1);
-            }
         }
         //randomly deprioritize new spaced items
         if (!item.last && item.next - now > 600) {
@@ -59,7 +42,8 @@ self.addEventListener('message', function(event) {
         }
         item.readiness = readiness;
         return -item.readiness;
+
     });
     //results
-    self.postMessage({schedule: data, spacedItems: spacedItems});
+    self.postMessage(data);
 }, false);
