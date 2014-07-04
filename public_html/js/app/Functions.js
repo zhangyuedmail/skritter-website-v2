98073c70bbb6d6bf4247e9a57c5a7ac07a73d06b
Joshua McFarland <mcfarljw@gmail.com>
2014-07-03 17:10:18 -0700

    Calculating interval should be considered function

--- public_html/js/app/Functions.js
+++ public_html/js/app/Functions.js
@@ -11,6 +11,90 @@ define([
      */
     var bootstrap = Bootstrap;
     /**
+     * @method calculateInterval
+     * @param {Object} item
+     * @param {Number} score
+     * @param {Object} srsconfigs
+     * @returns {Number}
+     */
+    var calculateInterval = function(item, score, srsconfigs) {
+            var newInterval = null;
+            //return new items with randomized default config values
+            if (!item.last) {
+                switch (score) {
+                    case 1:
+                        newInterval = srsconfigs.initialWrongInterval;
+                        break;
+                    case 2:
+                        newInterval = srsconfigs.initialRightInterval / 5;
+                        break;
+                    case 3:
+                        newInterval = srsconfigs.initialRightInterval;
+                        break;
+                    case 4:
+                        newInterval = srsconfigs.initialRightInterval * 4;
+                        break;
+                }
+                return randomInterval(newInterval);
+            }
+            //set values for further calculations
+            var actualInterval = getUnixTime() - item.last;
+            var factor;
+            var pctRight = item.successes / item.reviews;
+            var scheduledInterval = item.next - item.last;
+            //get the factor
+            if (score === 2) {
+                factor = 0.9;
+            } else if (score === 4) {
+                factor = 3.5;
+            } else {
+                var factorsList = (score === 1) ? srsconfigs.wrongFactors : srsconfigs.rightFactors;
+                var divisions = [2, 1200, 18000, 691200];
+                var index;
+                for (var i in divisions) {
+                    if (item.interval > divisions[i]) {
+                        index = i;
+                    }
+                }
+                factor = factorsList[index];
+            }
+            //adjust the factor based on readiness
+            if (score > 2) {
+                factor -= 1;
+                factor *= actualInterval / scheduledInterval;
+                factor += 1;
+            }
+            //accelerate new items that appear to be known
+            if (item.successes === item.reviews && item.reviews < 5) {
+                factor *= 1.5;
+            }
+            //decelerate hard items consistently marked wrong
+            if (item.reviews > 8) {
+                if (pctRight < 0.5) {
+                    factor *= Math.pow(pctRight, 0.7);
+                }
+            }
+            //multiple by the factor and randomize the interval
+            newInterval = randomInterval(item.interval * factor);
+            //bound the interval
+            if (score === 1) {
+                if (newInterval > 604800) {
+                    newInterval = 604800;
+                } else if (newInterval < 30) {
+                    newInterval = 30;
+                }
+            } else {
+                if (newInterval > 315569260) {
+                    newInterval = 315569260;
+                } else if (score === 2 && newInterval < 300) {
+                    newInterval = 300;
+                } else if (newInterval < 30) {
+                    newInterval = 30;
+                }
+            }
+            return newInterval;
+    };
+    /**
      * @method convertArrayToInt
      * @param {Array} array
      * @returns {Array}
@@ -270,6 +354,7 @@ define([
     
     return {
         bootstrap: bootstrap,
+        calculateInterval: calculateInterval,
         convertArrayToInt: convertArrayToInt,
         convertBytesToSize: convertBytesToSize,
         getAngle: getAngle,
