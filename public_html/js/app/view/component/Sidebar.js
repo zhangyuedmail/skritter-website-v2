90414998893b2ffb37095955196d2cc65285844a
Joshua McFarland <mcfarljw@gmail.com>
2014-07-14 10:06:38 -0400

    Pull sidebar into a view that is separately loaded

---
+++ public_html/js/app/view/component/Sidebar.js
@@ -0,0 +1,72 @@
+define([
+    'require.text!template/sidebar.html',
+    'view/View'
+], function(template, View) {
+    /**
+     * @class Sidebar
+     */
+    var Sidebar = View.extend({
+        /**
+         * @method initialize
+         */
+        initialize: function() {
+            View.prototype.initialize.call(this);
+        },
+        /**
+         * @method render
+         * @returns {Sidebar}
+         */
+        render: function() {
+            this.$el.html(_.template(template, skritter.strings));
+            this.$el.find('.app-version').text(skritter.getVersion());
+            $('.content-container').on('vclick', _.bind(this.handleContentContainerClick, this));
+            $('.navbar-sidebar-toggle').on('vclick', _.bind(this.handleToggle, this));
+            return this;
+        },
+
+        /**
+         * @property {Object} events
+         */
+        events: function() {
+            return _.extend({}, View.prototype.events, {
+                'vclick .button-sidebar-account': 'handleAccountClick',
+                'vclick .button-sidebar-home': 'handleHomeClick',
+                'vclick .button-sidebar-lists': 'handleListClick',
+                'vclick .button-sidebar-logout': 'handleLogoutClick',
+                'vclick .button-sidebar-study': 'handleStudyClick'
+            });
+        },
+        /**
+         * @method handleContentContainerClicked
+         * @param {Object} event
+         */
+        handleContentContainerClick: function(event) {
+            if (this.$el.hasClass('expanded')) {
+                this.toggle();
+                event.preventDefault();
+            }
+        },
+        /**
+         * @method handleSidebarToggle
+         * @param {Object} event
+         */
+        handleToggle: function(event) {
+            this.toggle();
+            event.preventDefault();
+        },
+        /**
+         * @method toggle
+         */
+        toggle: function() {
+            if (this.$el.hasClass('expanded')) {
+                this.$el.removeClass('expanded');
+                this.$el.hide('slide', {direction: 'left'}, 300);
+            } else {
+                this.$el.addClass('expanded');
+                this.$el.show('slide', {direction: 'left'}, 300);
+            }
+        }
+    });
+
+    return Sidebar;
+});
