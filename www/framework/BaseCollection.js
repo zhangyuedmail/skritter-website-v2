1b7bfaa02c0ca85ea60d9a3487f1ea9556849c47
mcfarljw <mcfarljw@gmail.com>
2014-09-15 22:04:38 -0500

    Added new base collection lazy add function

--- www/framework/BaseCollection.js
+++ www/framework/BaseCollection.js
@@ -6,5 +6,23 @@ define([], function() {
      * @class BaseCollection
      * @extends Backbone.Collection
      */
-    return Backbone.Collection.extend({});
+    return Backbone.Collection.extend({
+        /**
+         * @method lazyAdd
+         * @param {Array|Object} models
+         * @param {Function} callback
+         * @param {Object} [options]
+         */
+        lazyAdd: function(models, callback, options) {
+            models = Array.isArray(models) ? models : [models];
+            (function add(self) {
+                self.add(models.splice(0, 199), options);
+                if (models.length > 0) {
+                    setTimeout(add, 0, self);
+                } else {
+                    callback();
+                }
+            })(this);
+        }
+    });
 });
