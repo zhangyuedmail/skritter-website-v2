cab781e0aba60e0550e216648c0d137532f4fff8
mcfarljw <mcfarljw@gmail.com>
2014-10-03 11:34:01 -0500

    Updated decomps to attempt using user target language definitions

--- www/application/models/data/DataDecomp.js
+++ www/application/models/data/DataDecomp.js
@@ -21,16 +21,34 @@ define([
         getChildrenRows: function() {
             var decompHTML = '';
             var children = this.get('Children');
+            var definitions = this.getDefinitions();
             for (var i = 0, length = children.length; i < length; i++) {
                 var child = children[i];
                 decompHTML += '<tr>';
                 decompHTML += "<td class='writing character-font'>" + child.writing + '</td>';
                 decompHTML += "<td class='reading'>" + app.user.isChinese() ? app.fn.pinyin.toTone(child.reading) : child.reading + '</td>';
-                //TODO: fix this to support other languages
-                decompHTML += "<td class='definition'>" + child.definitions.en + '</td>';
+                decompHTML += "<td class='definition'>" + definitions[i] + '</td>';
                 decompHTML += '</tr>';
             }
             return decompHTML;
+        },
+        /**
+         * @method getDefinitions
+         * @returns {Array}
+         */
+        getDefinitions: function() {
+            var definitions = [];
+            var children = this.get('Children');
+            for (var i = 0, length = children.length; i < length; i++) {
+                var child = children[i];
+                var targetDefinition = child.definitions[app.user.settings.get('sourceLang')];
+                if (targetDefinition) {
+                    definitions.push(targetDefinition);
+                } else {
+                    definitions.push(child.definitions.en);
+                }
+            }
+            return definitions;
         }
     });
 
