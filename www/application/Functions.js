4135ebb7a0bfb39d64f80b290930cc8c3b58648a
mcfarljw <mcfarljw@gmail.com>
2014-11-01 17:07:44 -0500

    Return an empty string if text to html text is false or undefined.

--- www/application/Functions.js
+++ www/application/Functions.js
@@ -51,81 +51,81 @@ define([
      * @returns {Number}
      */
     function calculateInterval(item, score, configs) {
-            var newInterval = null;
-            //return new items with randomized default config values
-            if (!item.last) {
-                switch (score) {
-                    case 1:
-                        newInterval = configs.initialWrongInterval;
-                        break;
-                    case 2:
-                        newInterval = configs.initialRightInterval / 5;
-                        break;
-                    case 3:
-                        newInterval = configs.initialRightInterval;
-                        break;
-                    case 4:
-                        newInterval = configs.initialRightInterval * 4;
-                        break;
-                }
-                return randomInterval(newInterval);
+        var newInterval = null;
+        //return new items with randomized default config values
+        if (!item.last) {
+            switch (score) {
+                case 1:
+                    newInterval = configs.initialWrongInterval;
+                    break;
+                case 2:
+                    newInterval = configs.initialRightInterval / 5;
+                    break;
+                case 3:
+                    newInterval = configs.initialRightInterval;
+                    break;
+                case 4:
+                    newInterval = configs.initialRightInterval * 4;
+                    break;
             }
-            //set values for further calculations
-            var actualInterval = moment().unix() - item.last;
-            var factor = 0.9;
-            var pctRight = item.successes / item.reviews;
-            var scheduledInterval = item.next - item.last;
-            //get the factor
-            if (score === 2) {
-                factor = 0.9;
-            } else if (score === 4) {
-                factor = 3.5;
-            } else {
-                var factorsList = (score === 1) ? configs.wrongFactors : configs.rightFactors;
-                var divisions = [2, 1200, 18000, 691200];
-                var index;
-                for (var i in divisions) {
-                    if (item.interval > divisions[i]) {
-                        index = i;
-                    }
-                }
-                factor = factorsList[index];
-            }
-            //adjust the factor based on readiness
-            if (score > 2) {
-                factor -= 1;
-                factor *= actualInterval / scheduledInterval;
-                factor += 1;
-            }
-            //accelerate new items that appear to be known
-            if (item.successes === item.reviews && item.reviews < 5) {
-                factor *= 1.5;
-            }
-            //decelerate hard items consistently marked wrong
-            if (item.reviews > 8) {
-                if (pctRight < 0.5) {
-                    factor *= Math.pow(pctRight, 0.7);
+            return randomInterval(newInterval);
+        }
+        //set values for further calculations
+        var actualInterval = moment().unix() - item.last;
+        var factor = 0.9;
+        var pctRight = item.successes / item.reviews;
+        var scheduledInterval = item.next - item.last;
+        //get the factor
+        if (score === 2) {
+            factor = 0.9;
+        } else if (score === 4) {
+            factor = 3.5;
+        } else {
+            var factorsList = (score === 1) ? configs.wrongFactors : configs.rightFactors;
+            var divisions = [2, 1200, 18000, 691200];
+            var index;
+            for (var i in divisions) {
+                if (item.interval > divisions[i]) {
+                    index = i;
                 }
             }
-            //multiple by the factor and randomize the interval
-            newInterval = randomInterval(item.interval * factor);
-            //bound the interval
-            if (score === 1) {
-                if (newInterval > 604800) {
-                    newInterval = 604800;
-                } else if (newInterval < 30) {
-                    newInterval = 30;
-                }
-            } else {
-                if (newInterval > 315569260) {
-                    newInterval = 315569260;
-                } else if (score === 2 && newInterval < 300) {
-                    newInterval = 300;
-                } else if (newInterval < 30) {
-                    newInterval = 30;
-                }
+            factor = factorsList[index];
+        }
+        //adjust the factor based on readiness
+        if (score > 2) {
+            factor -= 1;
+            factor *= actualInterval / scheduledInterval;
+            factor += 1;
+        }
+        //accelerate new items that appear to be known
+        if (item.successes === item.reviews && item.reviews < 5) {
+            factor *= 1.5;
+        }
+        //decelerate hard items consistently marked wrong
+        if (item.reviews > 8) {
+            if (pctRight < 0.5) {
+                factor *= Math.pow(pctRight, 0.7);
             }
-            return newInterval;
+        }
+        //multiple by the factor and randomize the interval
+        newInterval = randomInterval(item.interval * factor);
+        //bound the interval
+        if (score === 1) {
+            if (newInterval > 604800) {
+                newInterval = 604800;
+            } else if (newInterval < 30) {
+                newInterval = 30;
+            }
+        } else {
+            if (newInterval > 315569260) {
+                newInterval = 315569260;
+            } else if (score === 2 && newInterval < 300) {
+                newInterval = 300;
+            } else if (newInterval < 30) {
+                newInterval = 30;
+            }
+        }
+        return newInterval;
     }
     /**
      * @property bootstrap
@@ -360,11 +360,13 @@ define([
      * @param {String} text
      */
     var textToHTML = function(text) {
-        text = text.replace(/img:(http:\/\/\S+)/gi, '<img src="$1"/>')
+        if (text) {
+            return text.replace(/img:(http:\/\/\S+)/gi, '<img src="$1"/>')
                 .replace(/_([^ _][^_]*)_(?!\S{4})/gi, '<em>$1</em>')
                 .replace(/\n/gi, '<br/>')
                 .replace(/\*([^*]+)\*/gi, '<strong>$1</strong>');
-        return text;
+        }
+        return '';
     };
     /**
      * @method toLowerCase
