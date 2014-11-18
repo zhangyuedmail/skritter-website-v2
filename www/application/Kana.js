5cdc7a2433eef9f84220ffd6f5c185e8334e950e
mcfarljw <mcfarljw@gmail.com>
2014-11-17 19:46:56 -0600

    Merge and update changes made to kana.js by Jeremy.

--- www/application/Kana.js
+++ www/application/Kana.js
@@ -2191,16 +2191,6 @@ define([], function () {
         },
         {
             "lang": "ja",
-            "rune": "ー",
-            "kana": true,
-            "strokes": [
-                [
-                    [1, 0.2, 0.44, 1, 1, 0]
-                ]
-            ]
-        },
-        {
-            "lang": "ja",
             "rune": "☺",
             "kana": true,
             "strokes": [
