a430714716df2538b8714c2d1989cf3ce4a62be3
mcfarljw <mcfarljw@gmail.com>
2014-12-01 15:58:50 -0600

    Fixed dashboard issue with due counter not getting updated.

--- www/application/pages/Dashboard.js
+++ www/application/pages/Dashboard.js
@@ -191,6 +191,7 @@ define([
                         app.user.data.sync(0, callback, callback);
                     }
                 ], function() {
+                    app.user.schedule.updateFilter();
                     app.user.schedule.sortFilter();
                     app.dialogs.hide();
                 });
