14e980ac2e20656b088ecbe8f676f038554ee5fc
mcfarljw <mcfarljw@gmail.com>
2015-03-27 10:56:34 -0500

    Added dialog to scratchpad while waiting for vocab to load.

--- src/modules/pages/Scratchpad.js
+++ src/modules/pages/Scratchpad.js
@@ -32,6 +32,7 @@ define([
          */
         render: function() {
             this.renderTemplate(Template);
+            this.renderDialog();
             this.prompt.setElement(this.$('.prompt-container'));
             this.prompt.hide().render();
             this.prompt.grading.hide();
@@ -51,12 +52,15 @@ define([
          */
         load: function(writing) {
             var self = this;
+            this.dialog.show('loading-scratchpad');
+            this.$('.vocab-writing').text(writing);
             app.user.data.vocabs.fetchByQuery(writing, function(vocab) {
                 self.vocab = vocab;
-
                 self.renderPrompt();
+                self.dialog.hide();
             }, function(error) {
                 console.error(error);
+                self.dialog.hide();
             });
             return this;
         }
