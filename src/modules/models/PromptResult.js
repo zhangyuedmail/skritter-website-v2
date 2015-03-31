6d72447a9f4f3a9e7fd496fa287d345c3ee1587f
mcfarljw <mcfarljw@gmail.com>
2015-03-31 12:22:17 -0500

    Built time tracking functions directly into the prompt result model.

--- src/modules/models/PromptResult.js
+++ src/modules/models/PromptResult.js
@@ -18,12 +18,69 @@ define([
             return {
                 character: null,
                 complete: false,
-                reviewTime: 0,
+                reviewingStart: 0,
+                reviewingStop: 0,
                 score: 3,
                 submitTime: Moment().unix(),
-                thinkingTime: 0,
+                thinkingStop: 0,
                 vocabId: null
             };
+        },
+        /**
+         * @method getReviewingTime
+         * @returns {Number}
+         */
+        getReviewingTime: function() {
+            return (this.get('reviewingStop') - this.get('reviewingStart')) / 1000;
+        },
+        /**
+         * @method getThinkingTime
+         * @returns {Number}
+         */
+        getThinkingTime: function() {
+            return (this.get('thinkingStop') - this.get('reviewingStart')) / 1000;
+        },
+        /**
+         * @method start
+         * @returns {PromptResult}
+         */
+        start: function() {
+            if (this.get('reviewingStart') === 0) {
+                this.set('reviewingStart', new Date().getTime());
+            }
+            return this;
+        },
+        /**
+         * @method stop
+         * @returns {PromptResult}
+         */
+        stop: function() {
+            var timestamp = new Date().getTime();
+            this.stopReviewing(timestamp);
+            this.stopThinking(timestamp);
+            return this;
+        },
+        /**
+         * @method stopReviewing
+         * @param {Number} [timestamp]
+         * @returns {PromptResult}
+         */
+        stopReviewing: function(timestamp) {
+            if (this.get('reviewingStop') === 0) {
+                this.set('reviewingStop', timestamp || new Date().getTime());
+            }
+            return this;
+        },
+        /**
+         * @method stopThinking
+         * @param {Number} [timestamp]
+         * @returns {PromptResult}
+         */
+        stopThinking: function(timestamp) {
+            if (this.get('thinkingStop') === 0) {
+                this.set('thinkingStop', timestamp || new Date().getTime());
+            }
+            return this;
         }
     });
 
