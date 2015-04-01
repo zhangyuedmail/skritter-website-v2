3981bf4faeb42ce751b762ea65848eeb4fa9edbf
mcfarljw <mcfarljw@gmail.com>
2015-04-01 15:23:30 -0500

    Updated data item load method to better handle variation ids.

--- src/modules/models/DataItem.js
+++ src/modules/models/DataItem.js
@@ -19,6 +19,8 @@ define([
          */
         initialize: function(attributes, options) {
             options = options || {};
+            this.contained = [];
+            this.decomps = [];
             this.strokes = [];
             this.vocabs = [];
         },
@@ -44,6 +46,17 @@ define([
             return this.getVocab().get('writing').split('');
         },
         /**
+         * @method getVocab
+         * @returns {DataVocab}
+         */
+        getVocab: function() {
+            var vocabs = this.vocabs;
+            if (app.user.isChinese()) {
+                return vocabs[this.get('reviews') % vocabs.length];
+            }
+            return vocabs[0];
+        },
+        /**
          * @method isNew
          * @returns {Boolean}
          */
@@ -65,6 +78,8 @@ define([
         load: function(callbackSuccess, callbackError) {
             var self = this;
             var options = {merge: true, silent: true, sort: false};
+            var part = this.get('part');
+            var userId = app.user.id;
             var vocabIds = self.get('vocabIds');
             Async.series([
                 //vocabs
@@ -72,17 +87,50 @@ define([
                     app.user.storage.get('vocabs', vocabIds, function(result) {
                         self.vocabs = app.user.data.vocabs.add(result, options);
                         callback();
-                    }, function(error) {
-                        callback('Unable to load vocabs.', error);
+                    }, function() {
+                        callback(new Error('Unable to load vocabs.'));
                     });
+                },
+                //contained items
+                function(callback) {
+                    var containedVocabIds = self.getVocab().get('containedVocabIds');
+                    if (containedVocabIds && part !== 'defn') {
+                        var contained = [];
+                        for (var i = 0, length = containedVocabIds.length; i < length; i++) {
+                            var splitId = containedVocabIds[i].split('-');
+                            var fallbackId = [userId, splitId[0], splitId[1], 0, part].join('-');
+                            var intendedId = [userId, containedVocabIds[i], part].join('-');
+                            if (self.collection.get(intendedId)) {
+                                contained.push(self.collection.get(intendedId));
+                            } else if (self.collection.get(fallbackId)) {
+                                contained.push(self.collection.get(fallbackId));
+                            } else {
+                                callback(new Error('Unable to load contained item ids.'));
+                                return;
+                            }
+                        }
+                        self.contained = contained;
+                        callback();
+                    } else {
+                        callback();
+                    }
                 },
                 //strokes
                 function(callback) {
                     app.user.storage.get('strokes', self.getCharacters(), function(result) {
                         self.strokes = app.user.data.strokes.add(result, options);
                         callback();
-                    }, function(error) {
-                        callback('Unable to load strokes.', error);
+                    }, function() {
+                        callback(new Error('Unable to load strokes.'));
+                    });
+                },
+                //decomps
+                function(callback) {
+                    app.user.storage.get('decomps', self.getCharacters(), function(result) {
+                        self.decomps = app.user.data.decomps.add(result, options);
+                        callback();
+                    }, function() {
+                        callback(new Error('Unable to load decomps.'));
                     });
                 }
             ], function(error) {
@@ -94,15 +142,13 @@ define([
             });
         },
         /**
-         * @method getVocab
-         * @returns {DataVocab}
+         * @method update
+         * @param {PromptResult} result
+         * @returns {DataItem}
          */
-        getVocab: function() {
-            var vocabs = this.vocabs;
-            if (app.user.isChinese()) {
-                return vocabs[this.get('reviews') % vocabs.length];
-            }
-            return vocabs[0];
+        update: function(result) {
+            this.set('next', 999999999999);
+            return this;
         }
     });
 
