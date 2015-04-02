6f41c737a1e41c00c4c4e1e718ef30588116e831
mcfarljw <mcfarljw@gmail.com>
2015-04-02 09:55:04 -0500

    Cleaned up all formatting on dashboard with cal heatmap.

--- src/modules/pages/Dashboard.js
+++ src/modules/pages/Dashboard.js
@@ -4,8 +4,9 @@
  */
 define([
     'require.text!templates/dashboard.html',
-    'core/modules/GelatoPage'
-], function(Template, GelatoPage) {
+    'core/modules/GelatoPage',
+    'modules/components/TableViewer'
+], function(Template, GelatoPage, TableViewer) {
 
     /**
      * @class PageDashboard
@@ -17,6 +18,9 @@ define([
          * @constructor
          */
         initialize: function() {
+            this.heatmap = new CalHeatMap();
+            this.lists = [];
+            this.tableLists = new TableViewer();
             this.listenTo(app.user.data.items, 'download:complete', $.proxy(this.updateDownloadProgress, this));
             this.listenTo(app.user.data.items, 'download:update', $.proxy(this.updateDownloadProgress, this));
         },
@@ -31,8 +35,11 @@ define([
          */
         render: function() {
             this.renderTemplate(Template);
+            this.tableLists.setElement(this.$('.progress-table-container')).render();
             this.renderDialog();
             this.renderFields();
+            this.renderHeatmap();
+            this.load();
             return this;
         },
         /**
@@ -45,6 +52,41 @@ define([
             } else {
                 this.$('#download-progress').hide();
             }
+            return this;
+        },
+        /**
+         * @method renderHeatmap
+         * @returns {PageDashboard}
+         */
+        renderHeatmap: function() {
+            this.heatmap.init({
+                cellRadius: 25,
+                cellSize: 25,
+                cellPadding: 5,
+                //data: 'datas-years.json',
+                domain: 'month',
+                domainDynamicDimension: false,
+                domainGutter: 20,
+                itemSelector: '.heatmap-container',
+                legend: [20, 40, 60, 80],
+                range: 1,
+                start: new Date(2015, 3, 1),
+                subDomain: 'x_day',
+                subDomainTextFormat: '%d'
+            });
+            return this;
+        },
+        /**
+         * @method renderTables
+         * @returns {PageDashboard}
+         */
+        renderTables: function() {
+            this.tableLists.set(this.lists, {
+                name: {title: '', type: 'text'},
+                progress: {title: '', type: 'progress'},
+                studyingMode: {title: '', type: 'text'},
+                addToQueue: {title: '', type: 'link', linkText: "<i class='fa fa-close'></i>"}
+            }, {showHeaders: false}).sortBy('name');
             return this;
         },
         /**
@@ -61,6 +103,19 @@ define([
         handleClickConfirmLogout: function(event) {
             event.preventDefault();
             app.user.logout();
+        },
+        /**
+         * @method load
+         * @returns {PageDashboard}
+         */
+        load: function() {
+            var self = this;
+            app.api.fetchVocabLists({sort: 'studying'}, function(result) {
+                self.lists = result.VocabLists || [];
+                self.renderTables();
+            }, function(error) {
+                console.log(error);
+            });
         },
         /**
          * @method updateDownloadProgress
