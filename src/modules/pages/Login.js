c6a2bd98ac007a534b09664cc1f672782f0f2235
mcfarljw <mcfarljw@gmail.com>
2015-04-20 14:48:28 +0900

    Revert login page into a skeleton.

--- src/modules/pages/Login.js
+++ src/modules/pages/Login.js
@@ -21,7 +21,7 @@ define([
          * @property title
          * @type String
          */
-        title: i18n.login.title + ' - ' + i18n.global.title,
+        title: 'Login - ' + i18n.global.title,
         /**
          * @method render
          * @returns {PageLogin}
