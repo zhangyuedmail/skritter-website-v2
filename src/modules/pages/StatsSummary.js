0010e3dbddcf77f56f1fc7efbd3c4b5031ef63d7
mcfarljw <mcfarljw@gmail.com>
2015-04-20 14:55:17 +0900

    Revert stats-summary page into a skeleton.

---
+++ src/modules/pages/StatsSummary.js
@@ -0,0 +1,37 @@
+/**
+ * @module Application
+ * @submodule Pages
+ */
+define([
+    'require.text!templates/stats-summary.html',
+    'core/modules/GelatoPage'
+], function(Template, GelatoPage) {
+
+    /**
+     * @class PageStats
+     * @extends GelatoPage
+     */
+    var PageStats = GelatoPage.extend({
+        /**
+         * @method initialize
+         * @constructor
+         */
+        initialize: function() {},
+        /**
+         * @property title
+         * @type String
+         */
+        title: 'Stats Summary - ' + i18n.global.title,
+        /**
+         * @method render
+         * @returns {PageStats}
+         */
+        render: function() {
+            this.renderTemplate(Template);
+            return this;
+        }
+    });
+
+    return PageStats;
+
+});
