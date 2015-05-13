825a0067cdbdfb09641003a8ef6ae8dfea58da2e
mcfarljw <mcfarljw@gmail.com>
2015-05-13 18:50:33 -0500

    Update local raygun library to version 1.18.3.

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ gelato.addPaths({
     'createjs.tween': 'libraries/createjs.tween-NEXT.min',
     'd3': 'libraries/d3-3.5.5.min',
     'heatmap': 'libraries/heatmap-3.5.2.min',
-    'raygun': 'libraries/raygun-1.18.2.min'
+    'raygun': 'libraries/raygun-1.18.3.min'
 });
 
 gelato.addShim({
