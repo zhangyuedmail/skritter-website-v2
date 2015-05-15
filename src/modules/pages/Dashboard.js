bd6c81ee5f60800020cad7bf968c4b3c9cdf7490
mcfarljw <mcfarljw@gmail.com>
2015-05-15 09:44:53 -0500

    Styled top section of the user dashboard.

--- src/modules/pages/Dashboard.js
+++ src/modules/pages/Dashboard.js
@@ -19,6 +19,8 @@ define([
          */
         initialize: function() {
             this.doughnutGoal = null;
+            this.doughnutList = null;
+            this.heatmap = new CalHeatMap();
             this.listQueue = new ListTable();
             this.listenTo(app.user.data.stats, 'add change', this.renderStats);
             this.listenTo(app.user.data.stats, 'add change', this.renderStats);
@@ -42,7 +44,10 @@ define([
         render: function() {
             this.renderTemplate(Template);
             this.listQueue.setElement(this.$('#list-queue-table')).render();
+            this.$('#section-expired').hide();
+            this.renderHeatmap();
             this.renderGoalDoughnut();
+            this.renderListDoughnut();
             this.renderListQueue();
             this.renderStats();
             return this;
@@ -55,49 +60,52 @@ define([
             var context = this.$('#goal-doughnut').get(0).getContext('2d');
             this.doughnutGoal = new Chart(context).Doughnut(
                 [
-                    {
-                        value: 60,
-                        color:"#F7464A",
-                        highlight: "#FF5A5E",
-                        label: "Red"
-                    },
-                    {
-                        value: 40,
-                        color: "#46BFBD",
-                        highlight: "#5AD3D1",
-                        label: "Green"
-                    }
+                    {value: 60, color:'#c5da4b'},
+                    {value: 40, color: '#efeef3'}
                 ],
                 {
-                    //Boolean - Whether we should show a stroke on each segment
-                    segmentShowStroke : true,
-
-                    //String - The colour of each segment stroke
-                    segmentStrokeColor : "#fff",
-
-                    //Number - The width of each segment stroke
-                    segmentStrokeWidth : 2,
-
-                    //Number - The percentage of the chart that we cut out of the middle
-                    percentageInnerCutout : 80, // This is 0 for Pie charts
-
-                    //Number - Amount of animation steps
-                    animationSteps : 100,
-
-                    //String - Animation easing effect
-                    animationEasing : "easeOutBounce",
-
-                    //Boolean - Whether we animate the rotation of the Doughnut
-                    animateRotate : true,
-
-                    //Boolean - Whether we animate scaling the Doughnut from the centre
-                    animateScale : false,
-
-                    //String - A legend template
-                    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-
+                    percentageInnerCutout : 80,
+                    animateRotate : false
                 }
             );
+        },
+        /**
+         * @method renderListDoughnut
+         * @returns {PageDashboard}
+         */
+        renderListDoughnut: function() {
+            var context = this.$('#list-queue-doughnut').get(0).getContext('2d');
+            this.doughnutList = new Chart(context).Doughnut(
+                [
+                    {value: 20, color:'#c5da4b'},
+                    {value: 80, color: '#efeef3'}
+                ],
+                {
+                    percentageInnerCutout : 80,
+                    animateRotate : false
+                }
+            );
+        },
+        /**
+         * @method renderHeatmap
+         * @returns {PageDashboard}
+         */
+        renderHeatmap: function() {
+            this.heatmap.init({
+                cellSize: 25,
+                cellPadding: 5,
+                cellRadius: 25,
+                domain: 'month',
+                domainDynamicDimension: false,
+                domainGutter: 20,
+                itemSelector: '#heatmap-container',
+                legend: [1, 50, 100, 200],
+                range: 1,
+                start: new Date(2015, new Date().getMonth(), 1),
+                subDomain: 'x_day',
+                subDomainTextFormat: '%d'
+            });
+            return this;
         },
         /**
          * @method renderListQueue
@@ -117,11 +125,11 @@ define([
          * @returns {PageDashboard}
          */
         renderStats: function() {
-            this.$('#characters-learned-value').text(app.user.data.stats.getTotalCharactersLearned());
-            this.$('#items-added-value').text(app.user.data.items.getAddedCount());
-            this.$('#items-reviewed-value').text(app.user.data.items.getReviewedCount());
-            this.$('#month-streak-value').text(app.user.data.stats.getStreak());
-            this.$('#words-learned-value').text(app.user.data.stats.getTotalWordsLearned());
+            this.$('#characters-learned .value').text(app.user.data.stats.getTotalCharactersLearned());
+            this.$('#items-added .value').text(app.user.data.items.getAddedCount());
+            this.$('#items-reviewed .value').text(app.user.data.items.getReviewedCount());
+            this.$('#month-streak .value').text(app.user.data.stats.getStreak());
+            this.$('#words-learned .value').text(app.user.data.stats.getTotalWordsLearned());
             return this;
         }
     });
