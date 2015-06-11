5ec4ae556a3ab5ccafcd6f5d2220d86db6deb155
mcfarljw <mcfarljw@gmail.com>
2015-06-11 14:43:47 -0500

    Made interval quantifier only accept json item objects.

--- src/modules/utils/IntervalQuantifier.js
+++ src/modules/utils/IntervalQuantifier.js
@@ -22,10 +22,9 @@ define([], function() {
      */
     IntervalQuantifier.prototype.quantify = function(item, score) {
         var newInterval = 0;
-        var itemObject = item.toJSON();
         var now = Moment().unix();
         //return new items with randomized default config values
-        if (!itemObject.last) {
+        if (!item.last) {
             switch (score) {
                 case 1:
                     newInterval = this.initialWrongInterval;
@@ -43,10 +42,10 @@ define([], function() {
             return this.randomizeInterval(newInterval);
         }
         //set values for further calculations
-        var actualInterval = now - itemObject.last;
+        var actualInterval = now - item.last;
         var factor = 0.9;
-        var pctRight = itemObject.successes / itemObject.reviews;
-        var scheduledInterval = itemObject.next - itemObject.last;
+        var pctRight = item.successes / item.reviews;
+        var scheduledInterval = item.next - item.last;
         //get the factor
         if (score === 2) {
             factor = 0.9;
@@ -57,7 +56,7 @@ define([], function() {
             var divisions = [2, 1200, 18000, 691200];
             var index;
             for (var i in divisions) {
-                if (itemObject.interval > divisions[i]) {
+                if (item.interval > divisions[i]) {
                     index = i;
                 }
             }
@@ -70,17 +69,17 @@ define([], function() {
             factor += 1;
         }
         //accelerate new items that appear to be known
-        if (itemObject.successes === itemObject.reviews && itemObject.reviews < 5) {
+        if (item.successes === item.reviews && item.reviews < 5) {
             factor *= 1.5;
         }
         //decelerate hard items consistently marked wrong
-        if (itemObject.reviews > 8) {
+        if (item.reviews > 8) {
             if (pctRight < 0.5) {
                 factor *= Math.pow(pctRight, 0.7);
             }
         }
         //multiple by the factor and randomize the interval
-        newInterval = this.randomizeInterval(itemObject.interval * factor);
+        newInterval = this.randomizeInterval(item.interval * factor);
         //bound the interval
         if (score === 1) {
             if (newInterval > 604800) {
