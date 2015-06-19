16321cf241771ac04978f1d55a866257bba81746
mcfarljw <mcfarljw@gmail.com>
2015-06-18 20:25:41 -0500

    Added keypress library to project for faster grading selection.

--- src/config.js
+++ src/config.js
@@ -12,6 +12,7 @@ gelato.addPaths({
     'createjs.tween': 'libraries/createjs.tween-NEXT.min',
     'd3': 'libraries/d3-3.5.5.min',
     'heatmap': 'libraries/heatmap-3.5.2.min',
+    'keypress': 'libraries/keypress-2.1.0.min',
     'raygun': 'libraries/raygun-1.18.3.min'
 });
 
--- src/modules/Libraries.js
+++ src/modules/Libraries.js
@@ -3,10 +3,13 @@
  * @class Libraries
  */
 define([
+    'keypress',
     'bootstrap.datetimepicker',
     'chart',
     'createjs.easel',
     'createjs.tween',
     'd3',
     'heatmap'
-]);
+], function(Keypress) {
+    window.Keypress = Keypress;
+});
