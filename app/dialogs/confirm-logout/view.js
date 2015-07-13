6c492173821116f57a862854d225e9a83b7bf570
mcfarljw <mcfarljw@gmail.com>
2015-07-13 12:53:47 -0500

    Added base dialogs for banning and logout confirmation.

---
+++ app/dialogs/confirm-logout/view.js
@@ -0,0 +1,45 @@
+var GelatoDialog = require('gelato/modules/dialog');
+
+/**
+ * @class ConfirmLogoutDialog
+ * @extends {GelatoDialog}
+ */
+module.exports = GelatoDialog.extend({
+    /**
+     * @property template
+     * @type {Function}
+     */
+    template: require('dialogs/confirm-logout/template'),
+    /**
+     * @method render
+     * @returns {ConfirmLogoutDialog}
+     */
+    render: function() {
+        this.renderTemplate();
+        return this;
+    },
+    /**
+     * @property events
+     * @type {Object}
+     */
+    events: {
+        'vclick #button-cancel': 'handleClickButtonCancel',
+        'vclick #button-logout': 'handleClickButtonLogout'
+    },
+    /**
+     * @method handleClickButtonCancel
+     * @param {Event} event
+     */
+    handleClickButtonCancel: function(event) {
+        event.preventDefault();
+        app.closeDialog();
+    },
+    /**
+     * @method handleClickButtonLogout
+     * @param {Event} event
+     */
+    handleClickButtonLogout: function(event) {
+        event.preventDefault();
+        app.closeDialog();
+    }
+});
