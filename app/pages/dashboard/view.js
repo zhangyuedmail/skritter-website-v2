0c9005dc28193bd85681357cdc4b45e5fe0c8419
mcfarljw <mcfarljw@gmail.com>
2015-07-14 13:10:28 -0500

    Added new vocablist table component to dashboard.

--- app/pages/dashboard/view.js
+++ app/pages/dashboard/view.js
@@ -2,6 +2,7 @@ var GelatoPage = require('gelato/modules/page');
 var DashboardGoal = require('components/dashboard-goal/view');
 var DashboardMonth = require('components/dashboard-month/view');
 var DashboardTotal = require('components/dashboard-total/view');
+var VocablistTable = require('components/vocablist-table/view');
 
 /**
  * @class Dashboard
@@ -16,6 +17,7 @@ module.exports = GelatoPage.extend({
         this.dashboardGoal = new DashboardGoal();
         this.dashboardMonth = new DashboardMonth();
         this.dashboardTotal = new DashboardTotal();
+        this.vocablistTable = new VocablistTable();
     },
     /**
      * @property title
@@ -36,9 +38,11 @@ module.exports = GelatoPage.extend({
         this.dashboardGoal.setElement('#dashboard-goal-container').render();
         this.dashboardMonth.setElement('#dashboard-month-container').render();
         this.dashboardTotal.setElement('#dashboard-total-container').render();
+        this.vocablistTable.setElement('#vocablist-table-container').render();
         app.user.data.items.fetchDaily();
         app.user.data.items.fetchNext();
         app.user.data.stats.fetch();
+        app.user.data.vocablists.fetch();
         return this;
     },
     /**
@@ -49,6 +53,7 @@ module.exports = GelatoPage.extend({
         this.dashboardGoal.remove();
         this.dashboardMonth.remove();
         this.dashboardTotal.remove();
+        this.vocablistTable.remove();
         return GelatoPage.prototype.remove.call(this);
     }
 });
