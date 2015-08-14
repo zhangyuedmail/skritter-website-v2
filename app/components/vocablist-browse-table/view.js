abbd23052cfa3505de4ce4729496f59c9163f60f
Scott Erickson <sderickson@gmail.com>
2015-08-13 17:46:28 -0700

    Clear out vestigial code from browse page view

--- app/components/vocablist-browse-table/view.js
+++ app/components/vocablist-browse-table/view.js
@@ -10,7 +10,6 @@ module.exports = GelatoComponent.extend({
      * @constructor
      */
     initialize: function() {
-        this.fields = {name: 'Name', progress: 'Progress'};
         this.lists = [];
         this.type = null;
         this.listenTo(app.user.data.vocablists, 'fetch', this.render);
@@ -46,123 +45,6 @@ module.exports = GelatoComponent.extend({
         this.delegateEvents();
         return this;
     },
-    /**
-     * @method renderTable
-     * @returns {VocablistTable}
-     */
-    renderTable: function() {
-        var tableBody = '';
-        var tableHead = '';
-        //table head
-        tableHead += "<tr>";
-        for (var field1 in this.fields) {
-            tableHead += "<th>";
-            tableHead += this.fields[field1];
-            tableHead += "</th>";
-        }
-        tableHead += "</tr>";
-        //table body
-        for (var i = 0, length = this.lists.length; i < length; i++) {
-            var list = this.lists[i];
-            tableBody += "<tr id='row-" + list.id + "' class='cursor'>";
-            for (var field2 in this.fields) {
-                tableBody += "<td class='field-" + field2.toLowerCase() + "'>";
-                switch (field2) {
-                    case 'addStatus':
-                        tableBody += this.getAddStatus({status: list.get('studyingMode')});
-                        break;
-                    case 'progress':
-                        tableBody += this.getProgressBar({value: list.getProgress()});
-                        break;
-                    case 'remove':
-                        tableBody += '<i class="fa fa-close"></i>';
-                        break;
-                    case 'status':
-                        tableBody += this.getStatus({status: list.get('studyingMode')});
-                        break;
-                    case 'startAdding':
-                        tableBody += '<a href="#">Restart adding</a>';
-                        break;
-                    case 'stopAdding':
-                        tableBody += '<a href="#">Stop adding</a>';
-                        break;
-                    case 'study':
-                        tableBody += '<a href="#">Study list</a>';
-                        break;
-                    default:
-                        tableBody += list.get(field2);
-                }
-                tableBody += "</td>";
-            }
-            tableBody += "</tr>";
-        }
-        this.$('table tbody').html(tableBody);
-        this.$('table thead').html(tableHead);
-        this.renderEvents();
-        return this;
-    },
-    /**
-     * @method getAddStatus
-     * @param {Object} [options]
-     * @returns {String}
-     */
-    getAddStatus: function(options) {
-        options = options || {};
-        var status = options.status;
-        switch (status) {
-            case 'adding':
-                return 'Adding';
-            case 'finished':
-                return 'Finished';
-            case 'not studying':
-                return '<i class="fa fa-plus-circle"></i> Add to queue';
-            case 'reviewing':
-                return 'Reviewing';
-            default:
-                return 'Unknown';
-        }
-    },
-    /**
-     * @method getProgressBar
-     * @param {Object} [options]
-     * @returns {String}
-     */
-    getProgressBar: function(options) {
-        options = options || {};
-        var value = options.value || 0;
-        return [
-            '<div class="progress">',
-            '<div class="progress-bar" role="progressbar" style="width: ' + value + '%;">',
-            '</div></div>'
-        ].join('');
-
-    },
-    /**
-     * @method getStatus
-     * @param {Object} [options]
-     * @returns {String}
-     */
-    getStatus: function(options) {
-        options = options || {};
-        var status = options.status;
-        switch (status) {
-            case 'adding':
-                return 'Adding';
-            case 'finished':
-                return 'Finished';
-            case 'not studying':
-                return 'Not Studying';
-            case 'reviewing':
-                return 'Reviewing';
-            default:
-                return 'Unknown';
-        }
-    },
-    /**
-     * @method setType
-     * @param {String} type
-     * @returns {VocablistTable}
-     */
     setType: function(type) {
         this.type = type;
         this.update();
