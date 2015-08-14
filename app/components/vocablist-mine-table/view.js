5bb5a186bee7aff18f525c9d4e8e550ab9a46797
Scott Erickson <sderickson@gmail.com>
2015-08-13 19:06:34 -0700

    Add pagination and add-to-queue to my lists

--- app/components/vocablist-mine-table/view.js
+++ app/components/vocablist-mine-table/view.js
@@ -13,19 +13,22 @@ module.exports = GelatoComponent.extend({
     initialize: function() {
         this.vocablists = new MyVocablists();
         this.listenTo(this.vocablists, 'sync', this.render);
-        this.vocablists.fetch({
+        this.fetchOptions = {
             headers: { 'Authorization': 'bearer '+app.api.getToken() },
-            context: self,
             data: {
+                limit: 10,
                 sort: 'custom'
             }
-        });
+        };
+        this.vocablists.fetch(this.fetchOptions);
     },
     /**
      * @property events
      * @typeof {Object}
      */
     events: {
+        'vclick #load-more-btn': 'handleClickLoadMoreButton',
+        'vclick .add-to-queue-link': 'handleClickAddToQueueLink'
     },
     /**
      * @property template
@@ -39,5 +42,39 @@ module.exports = GelatoComponent.extend({
     render: function() {
         this.renderTemplate();
         return this;
+    },
+    /**
+     * @method handleClickAddToQueueLink
+     * @param {Event} e
+     */
+    handleClickAddToQueueLink: function(e) {
+        var listID = $(e.target).closest('.add-to-queue-link').data('vocablist-id');
+        var vocablist = this.vocablists.get(listID);
+        if (vocablist.get('studyingMode') !== 'not studying')
+            return;
+
+        vocablist.set('studyingMode', 'adding');
+        vocablist.save();
+        this.render();
+    },
+    /**
+     * @method handleClickLoadMoreButton
+     * @param {Event} e
+     */
+    handleClickLoadMoreButton: function(e) {
+        if (!this.vocablists.cursor) {
+            return;
+        }
+        var moreVocabLists = new MyVocablists();
+        this.fetchOptions.data.cursor = this.vocablists.cursor;
+        this.fetchingMore = true;
+        this.listenToOnce(moreVocabLists, 'sync', function() {
+            this.vocablists.cursor = moreVocabLists.cursor;
+            this.vocablists.add(moreVocabLists.slice());
+            this.fetchingMore = false;
+            this.render();
+        });
+        moreVocabLists.fetch(this.fetchOptions);
+        this.render();
     }
 });
