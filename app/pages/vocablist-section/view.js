b2ff62ba1fbaac54d9e58905493a1d70336d4505
Scott Erickson <sderickson@gmail.com>
2015-08-28 14:53:22 -0700

    Add partially built action select to section page

--- app/pages/vocablist-section/view.js
+++ app/pages/vocablist-section/view.js
@@ -15,7 +15,6 @@ module.exports = GelatoPage.extend({
      * @type {String}
      */
     bodyClass: 'background1',
-
     /**
      * @method initialize
      * @constructor
@@ -29,9 +28,23 @@ module.exports = GelatoPage.extend({
         this.vocabs = new Vocabs();
         this.listenTo(this.vocablist, 'state', this.render);
         this.listenTo(this.section, 'state', this.handleSectionLoaded);
-        this.listenTo(this.vocabs, 'state', this.render);
+        this.listenTo(this.vocabs, 'state', this.renderTable);
         this.showTrad = app.user.get('reviewTraditional');
         this.showSimp = app.user.get('reviewSimplified') || this.vocablist.get('lang') === 'ja';
+    },
+    /**
+     * @method renderTable
+     */
+    renderTable: function() {
+        // this works
+        var context = require('globals');
+        context.view = this;
+        var rendering = $(this.template(context));
+
+        // this does not
+        //var rendering = $(this.template(require('globals')));
+
+        this.$('table').replaceWith(rendering.find('table'));
     },
     /**
      * @method handleSectionLoaded
@@ -72,6 +85,8 @@ module.exports = GelatoPage.extend({
      * @type {Object}
      */
     events: {
+        'vclick .checkbox-td': 'handleClickCheckboxTd',
+        'change #action-select': 'handleChangeActionSelect'
     },
     /**
      * @property title
@@ -99,5 +114,38 @@ module.exports = GelatoPage.extend({
     remove: function() {
         this.navbar.remove();
         return GelatoPage.prototype.remove.call(this);
+    },
+    /**
+     * Widens the click area to the table cell element.
+     * Disables/enables select box depending on if anything is selected.
+     * Keeps vocab.checked property up to date.
+     *
+     * @method handleClickTdCheckbox
+     * @param {Event} e
+     */
+    handleClickCheckboxTd: function(e) {
+        var checkbox = $(e.target);
+        if ($(e.target).is('.checkbox-td')) {
+            // propagate downward
+            $(e.target).find('input').click();
+            return;
+        }
+
+        var anyChecked = this.$('input:checked').length;
+        this.$('#action-select').attr('disabled', !anyChecked);
+        var vocabId = checkbox.closest('tr').data('vocab-id');
+        var vocab = this.vocabs.get(vocabId);
+        vocab.set('checked', checkbox.is(':checked'));
+    },
+    /**
+     * @method handleChangeActionSelect
+     */
+    handleChangeActionSelect: function(e) {
+        var action = $(e.target).val();
+        if (!action) {
+            return;
+        }
+        var selected = this.vocabs.filter({selected: true});
+        console.log('run action', action, 'on selected', selected);
     }
 });
