fdbf0c23c456c64b70dd4300735ebf29e7e13045
Scott Erickson <sderickson@gmail.com>
2015-09-25 17:01:46 -0700

    Revert back to loading 20 items at a time

--- app/pages/words-all/view.js
+++ app/pages/words-all/view.js
@@ -41,7 +41,7 @@ module.exports = GelatoPage.extend({
         this.vocabMap = {};
         this.initAllCollections();
         this.sort = 'last';
-        this.limit = 2;
+        this.limit = 20;
         this.fetchItems();
         this.searchString = '';
     },
