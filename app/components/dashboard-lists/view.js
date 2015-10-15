382dfd92dbcd4afb701eea9df2dd4e7dbddb40df
Joshua McFarland <mcfarljw@gmail.com>
2015-10-15 10:43:54 -0500

    Create a basic dashboard component for navigating lists.

---
+++ app/components/dashboard-lists/view.js
@@ -0,0 +1,68 @@
+var GelatoComponent = require('gelato/component');
+
+/**
+ * @class DashboardLists
+ * @extends {GelatoComponent}
+ */
+module.exports = GelatoComponent.extend({
+    /**
+     * @method initialize
+     * @constructor
+     */
+    initialize: function() {
+        this.vocablist = null;
+        this.vocablists = this.createCollection('collections/vocablists');
+        this.listenTo(this.vocablists, 'state', this.render);
+        this.vocablists.fetch({
+            data: {
+                limit: 10,
+                sort: 'adding',
+                include_percent_done: 'true',
+                lang: this.app.getLanguage()
+            }
+        });
+    },
+    /**
+     * @property events
+     * @type {Object} events
+     */
+    events: {
+        'click .button-sections': 'handleClickVocablistSections',
+        'click .button-show-lists': 'handleClickShowLists'
+    },
+    /**
+     * @property template
+     * @type {Function}
+     */
+    template: require('./template'),
+    /**
+     * @method render
+     * @returns {DashboardStatus}
+     */
+    render: function() {
+        this.renderTemplate();
+        return this;
+    },
+    /**
+     * @method handleClickVocablistSections
+     * @param {Event} event
+     */
+    handleClickVocablistSections: function(event) {
+        event.preventDefault();
+        var $row = $(event.target).closest('.row');
+        var attributes = {id: $row.data('list-id')};
+        this.vocablist = this.createModel('models/vocablist', attributes);
+        this.listenToOnce(this.vocablist, 'state:standby', this.render);
+        this.vocablist.fetch();
+    },
+    /**
+     * @method handleClickShowLists
+     * @param {Event} event
+     */
+    handleClickShowLists: function(event) {
+        event.preventDefault();
+        var $link =  $(event.target);
+        this.vocablist = null;
+        this.render();
+    }
+});
