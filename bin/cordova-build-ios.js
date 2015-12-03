7d24241d564cea42fe0e3bdb29c7950541e2c8a2
Joshua McFarland <mcfarljw@gmail.com>
2015-12-03 17:14:19 +0800

    Add bin scripts, base cordova directories and new dependencies.

---
+++ bin/cordova-build-ios.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var lodash = require('lodash');
+var minimist = require('minimist');
+var shell = require('shelljs');
+
+var argv = minimist(process.argv.slice(2));
+var path = process.cwd().toString();
+
+switch (argv.lang) {
+    case 'ja':
+        shell.cd(path + '/cordova/japanese');
+        break;
+    case 'zh':
+        shell.cd(path + '/cordova/chinese');
+        break;
+    default:
+        console.error('Unable to build specified language.');
+        process.exit(1);
+}
+
+shell.rm('-rf', './www');
+shell.cp('-rf', '../../public/*', './www');
+shell.exec('cordova build ios');
+
+process.exit(0);
