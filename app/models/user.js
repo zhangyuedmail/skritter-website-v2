a7964106a2e51f7d9992cb69202c6d529a57bd27
Joshua McFarland <mcfarljw@gmail.com>
2015-12-14 04:50:45 -0600

    Return a boolean value with user isLoggedIn method.

--- app/models/user.js
+++ app/models/user.js
@@ -142,7 +142,7 @@ module.exports = SkritterModel.extend({
      * @returns {Boolean}
      */
     isLoggedIn: function() {
-        return this.session.get('user_id');
+        return this.session.has('user_id');
     },
     /**
      * @method login
