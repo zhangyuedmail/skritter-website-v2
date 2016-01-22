645eef31b796d901d4bd919b93f3eaf566170c96
Joshua McFarland <mcfarljw@gmail.com>
2016-01-22 15:14:39 -0600

    Add an gelato dialog view for testing.

---
+++ app/base/gelato-dialog.js
@@ -0,0 +1,86 @@
+var GelatoView = require('gelato/view');
+
+/**
+ * @class GelatoDialog
+ * @extends {GelatoView}
+ */
+var GelatoDialog = GelatoView.extend({
+    /**
+     * @property el
+     * @type {String}
+     */
+    el: 'gelato-dialogs',
+    /**
+     * @property element
+     * @type {jQuery}
+     */
+    element: null,
+    /**
+     * @method renderTemplate
+     * @param {Object} [context]
+     * @returns {GelatoDialog}
+     */
+    renderTemplate: function(context) {
+        GelatoView.prototype.renderTemplate.call(this, context);
+        this.element = this.$('[role="dialog"]');
+        this.element.on('hide.bs.modal', this.handleElementHide.bind(this));
+        this.element.on('hidden.bs.modal', this.handleElementHidden.bind(this));
+        this.element.on('show.bs.modal', this.handleElementShow.bind(this));
+        this.element.on('shown.bs.modal', this.handleElementShown.bind(this));
+        return this;
+    },
+    /**
+     * @method close
+     * @returns {GelatoDialog}
+     */
+    close: function() {
+        this.element.modal('hide');
+        return this;
+    },
+    /**
+     * @method handleElementHide
+     */
+    handleElementHide: function() {
+        this.trigger('hide');
+    },
+    /**
+     * @method handleElementHidden
+     */
+    handleElementHidden: function() {
+        this.trigger('hidden');
+        this.remove();
+    },
+    /**
+     * @method handleElementShow
+     */
+    handleElementShow: function() {
+        this.trigger('show');
+    },
+    /**
+     * @method handleElementShown
+     */
+    handleElementShown: function() {
+        this.trigger('shown');
+    },
+    /**
+     * @method open
+     * @param {Object} [options]
+     * @returns {GelatoDialog}
+     */
+    open: function(options) {
+        _.defaults(
+            options || {},
+            {
+                backdrop: 'static',
+                keyboard: false,
+                show: true,
+                remote: false
+            }
+        );
+        this.render();
+        this.element.modal(options);
+        return this;
+    }
+});
+
+module.exports = GelatoDialog;
