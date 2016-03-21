b10a44cb8bd0bab09111e4a24793995c0441f34f
Michael Prokopchuk <prokopcm@gmail.com>
2016-03-21 11:06:42 -0400

    Improve readability of start.js

--- bin/start.js
+++ bin/start.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
-var os = require('os');
 var minimist = require('minimist');
 var shell = require('shelljs');
 
 var argv = minimist(process.argv.slice(2));
 var path = process.cwd().toString();
-var isWindows = /win/.test(os.platform());
+var cmd;
 
-var cmd = './node_modules/.bin/brunch watch --server';
-if (isWindows) {
-	cmd = 'node node_modules/brunch/bin/brunch watch --server';
+switch(process.platform) {
+    case 'win32':
+        cmd = 'node node_modules/brunch/bin/brunch watch --server';
+        break;
+    default:
+        cmd = './node_modules/.bin/brunch watch --server';
 }
+
 shell.exec(cmd);
 process.exit(0);
