dbb5642866977c50500031d5a53258b6bc13022a
Joshua McFarland <mcfarljw@gmail.com>
2016-03-30 12:15:02 -0500

    Use proper unicode character for japanese hyphen.

--- app/models/vocab.js
+++ app/models/vocab.js
@@ -366,7 +366,7 @@ var Vocab = SkritterModel.extend({
                 return true;
             }
         }
-        return _.includes(['~', 'ー', '～', '一'], this.get('writing'));
+        return _.includes(['~', '-', '～', 'ー'], this.get('writing'));
     },
     /**
      * @method isJapanese
