9d3c9ecab78305318010e66558390e20dafd84f7
Michael Prokopchuk <prokopcm@gmail.com>
2016-04-07 17:20:55 -0400

    Add heatmap chart, hook up to data from prev 30 days, improve style

--- app/components/stats/heatmap/view.js
+++ app/components/stats/heatmap/view.js
@@ -11,6 +11,7 @@ module.exports = GelatoComponent.extend({
      * @constructor
      */
     initialize: function(options) {
+        this.heatmap = new CalHeatMap();
         this.listenTo(this.collection, 'state:standby', this.update);
     },
     /**
@@ -24,9 +25,31 @@ module.exports = GelatoComponent.extend({
      */
     render: function() {
         this.renderTemplate();
+        this.renderGraph();
+
+        return this;
+    },
+
+    renderGraph: function() {
+        this.heatmap.init({
+            cellSize: 22,
+            cellPadding: 5,
+            cellRadius: 25,
+            domain: 'month',
+            domainDynamicDimension: false,
+            domainGutter: 20,
+            itemSelector: '#heatmap',
+            legend: [0, 50, 100, 200],
+            range: 1,
+            start: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+            subDomain: 'x_day',
+            subDomainTextFormat: "%d"
+        });
+
+        this.update();
     },
     
     update: function() {
-        
+        this.heatmap.update(this.collection.getMonthlyHeatmapData());
     }
 });
