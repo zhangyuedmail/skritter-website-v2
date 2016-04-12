8a128e8fd2782d88848e51d2531a0b7384df338f
Joshua McFarland <mcfarljw@gmail.com>
2016-04-12 16:36:05 -0500

    Update lodash and potentially simplify bin scripts.

--- bin/build.js
+++ bin/build.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 
-var lodash = require('lodash');
-var minimist = require('minimist');
 var shell = require('shelljs');
 
-var argv = minimist(process.argv.slice(2));
-var path = process.cwd().toString();
-
-shell.exec('./node_modules/.bin/brunch build --production');
-process.exit(0);
+shell.exec('node ./node_modules/brunch/bin/brunch build --production');
+shell.exit(0);
--- bin/start.js
+++ bin/start.js
@@ -1,19 +1,6 @@
 #!/usr/bin/env node
 
-var minimist = require('minimist');
 var shell = require('shelljs');
 
-var argv = minimist(process.argv.slice(2));
-var path = process.cwd().toString();
-var cmd;
-
-switch(process.platform) {
-    case 'win32':
-        cmd = 'node node_modules/brunch/bin/brunch watch --server';
-        break;
-    default:
-        cmd = './node_modules/.bin/brunch watch --server';
-}
-
-shell.exec(cmd);
-process.exit(0);
+shell.exec('node ./node_modules/brunch/bin/brunch watch --server');
+shell.exit(0);
