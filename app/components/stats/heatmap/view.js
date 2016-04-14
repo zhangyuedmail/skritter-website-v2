309983434acb047a436dc60e30e277d4fc2ac3cc
Michael Prokopchuk <prokopcm@gmail.com>
2016-04-14 11:54:39 -0400

    Update code formatting to match new project standards

--- app/components/stats/heatmap/view.js
+++ app/components/stats/heatmap/view.js
@@ -5,51 +5,51 @@ var GelatoComponent = require('gelato/component');
  * @extends {GelatoComponent}
  */
 module.exports = GelatoComponent.extend({
-    /**
-     *
-     * @method initialize
-     * @constructor
-     */
-    initialize: function(options) {
-        this.heatmap = new CalHeatMap();
-        this.listenTo(this.collection, 'state:standby', this.update);
-    },
-    /**
-     * @property template
-     * @type {Function}
-     */
-    template: require('./template'),
-    /**
-     * @method render
-     * @returns {VocablistSideBar}
-     */
-    render: function() {
-        this.renderTemplate();
-        this.renderGraph();
+  /**
+   *
+   * @method initialize
+   * @constructor
+   */
+  initialize: function(options) {
+    this.heatmap = new CalHeatMap();
+    this.listenTo(this.collection, 'state:standby', this.update);
+  },
+  /**
+   * @property template
+   * @type {Function}
+   */
+  template: require('./template'),
+  /**
+   * @method render
+   * @returns {VocablistSideBar}
+   */
+  render: function() {
+    this.renderTemplate();
+    this.renderGraph();
 
-        return this;
-    },
+    return this;
+  },
 
-    renderGraph: function() {
-        this.heatmap.init({
-            cellSize: 22,
-            cellPadding: 5,
-            cellRadius: 25,
-            domain: 'month',
-            domainDynamicDimension: false,
-            domainGutter: 20,
-            itemSelector: '#heatmap',
-            legend: [0, 50, 100, 200],
-            range: 1,
-            start: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
-            subDomain: 'x_day',
-            subDomainTextFormat: "%d"
-        });
+  renderGraph: function() {
+    this.heatmap.init({
+      cellSize: 22,
+      cellPadding: 5,
+      cellRadius: 25,
+      domain: 'month',
+      domainDynamicDimension: false,
+      domainGutter: 20,
+      itemSelector: '#heatmap',
+      legend: [0, 50, 100, 200],
+      range: 1,
+      start: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+      subDomain: 'x_day',
+      subDomainTextFormat: "%d"
+    });
 
-        this.update();
-    },
-    
-    update: function() {
-        this.heatmap.update(this.collection.getMonthlyHeatmapData());
-    }
+    this.update();
+  },
+
+  update: function() {
+    this.heatmap.update(this.collection.getMonthlyHeatmapData());
+  }
 });
--- app/pages/stats/view.js
+++ app/pages/stats/view.js
@@ -9,102 +9,102 @@ var ProgressStats = require('collections/progress-stats');
  * @extends {GelatoPage}
  */
 module.exports = GelatoPage.extend({
-    /**
-     * @method initialize
-     * @constructor
-     */
-    initialize: function() {
-        this.statsCollection = new ProgressStats();
+  /**
+   * @method initialize
+   * @constructor
+   */
+  initialize: function() {
+    this.statsCollection = new ProgressStats();
 
-        // TODO: better this
-        this.statsCollection.fetchMonth();
+    // TODO: better this
+    this.statsCollection.fetchMonth();
 
-        this._views = {};
-        this._views['navbar'] = new DefaultNavbar();
-        this._views['summary'] = new StatsSummaryComponent({
-            collection: this.statsCollection
-        });
-        this._views['timeline'] = new StatsTimelineComponent({
-            collection: this.statsCollection
-        });
+    this._views = {};
+    this._views['navbar'] = new DefaultNavbar();
+    this._views['summary'] = new StatsSummaryComponent({
+      collection: this.statsCollection
+    });
+    this._views['timeline'] = new StatsTimelineComponent({
+      collection: this.statsCollection
+    });
 
-        this.activeSection = 'summary';
-    },
+    this.activeSection = 'summary';
+  },
 
-    events: {
-        'click .stats-section-selector': 'handleStatsSectionSelectorClicked'
-    },
+  events: {
+    'click .stats-section-selector': 'handleStatsSectionSelectorClicked'
+  },
 
-    /**
-     * @property template
-     * @type {Function}
-     */
-    template: require('./template'),
+  /**
+   * @property template
+   * @type {Function}
+   */
+  template: require('./template'),
 
-    /**
-     * @property title
-     * @type {String}
-     */
-    title: 'Stats - Skritter',
+  /**
+   * @property title
+   * @type {String}
+   */
+  title: 'Stats - Skritter',
 
-    /**
-     * @method render
-     * @returns {Home}
-     */
-    render: function() {
-        this.renderTemplate();
-        this._views['navbar'].setElement('#navbar-container').render();
-        this._views['summary'].setElement('#stats-summary-container').render();
-        this._views['timeline'].setElement('#stats-timeline-container').render();
+  /**
+   * @method render
+   * @returns {Home}
+   */
+  render: function() {
+    this.renderTemplate();
+    this._views['navbar'].setElement('#navbar-container').render();
+    this._views['summary'].setElement('#stats-summary-container').render();
+    this._views['timeline'].setElement('#stats-timeline-container').render();
 
-        this.showStatsSection();
+    this.showStatsSection();
 
-        return this;
-    },
+    return this;
+  },
 
-    /**
-     * @method remove
-     * @returns {Home}
-     */
-    remove: function() {
-        for (var view in this._views) {
-            this._views[view].remove();
-        }
-        return GelatoPage.prototype.remove.call(this);
-    },
-
-    /**
-     *
-     * @param {String} [section] the section to show. Defaults to activeSection.
-     */
-    showStatsSection: function(section) {
-        var toShowSection = section || this.activeSection;
-        var toHideSection = toShowSection === 'summary' ? 'timeline' : 'summary';
-
-        this._views[toHideSection].hide();
-        this._views[toShowSection].show();
-
-        if (_.isFunction(this._views[toShowSection].onTabVisible)) {
-            this._views[toShowSection].onTabVisible();
-        }
-    },
-
-    /**
-     *
-     * @param {jQuery.Event} event a click event
-     */
-    handleStatsSectionSelectorClicked: function(event) {
-        event.preventDefault();
-        var newSection = event.target.id.split('-')[0];
-
-        if (newSection === this.activeSection) {
-            return;
-        }
-
-        this.$('#' + this.activeSection + '-selector').removeClass('active');
-        this.$('#' + newSection + '-selector').addClass('active');
-        this.activeSection = newSection;
-
-        this.showStatsSection();
+  /**
+   * @method remove
+   * @returns {Home}
+   */
+  remove: function() {
+    for (var view in this._views) {
+      this._views[view].remove();
     }
+    return GelatoPage.prototype.remove.call(this);
+  },
+
+  /**
+   *
+   * @param {jQuery.Event} event a click event
+   */
+  handleStatsSectionSelectorClicked: function(event) {
+    event.preventDefault();
+    var newSection = event.target.id.split('-')[0];
+
+    if (newSection === this.activeSection) {
+      return;
+    }
+
+    this.$('#' + this.activeSection + '-selector').removeClass('active');
+    this.$('#' + newSection + '-selector').addClass('active');
+    this.activeSection = newSection;
+
+    this.showStatsSection();
+  },
+
+  /**
+   *
+   * @param {String} [section] the section to show. Defaults to activeSection.
+   */
+  showStatsSection: function(section) {
+    var toShowSection = section || this.activeSection;
+    var toHideSection = toShowSection === 'summary' ? 'timeline' : 'summary';
+
+    this._views[toHideSection].hide();
+    this._views[toShowSection].show();
+
+    if (_.isFunction(this._views[toShowSection].onTabVisible)) {
+      this._views[toShowSection].onTabVisible();
+    }
+  }
 });
