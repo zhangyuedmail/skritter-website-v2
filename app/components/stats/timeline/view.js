c669206432a28f56c9ba04acaa7003e70f71ad4d
Michael Prokopchuk <prokopcm@gmail.com>
2016-04-18 22:07:51 -0400

    Update  unit labels based on time granularity, add documentation

--- app/components/stats/timeline/view.js
+++ app/components/stats/timeline/view.js
@@ -13,15 +13,24 @@ module.exports = GelatoComponent.extend({
    */
   initialize: function(options) {
     this._views = {};
+
+    // TODO: check localStorage for user-set granularity config?
     this._views['bargraph'] = new TimeStudiedBragraphComponent({
-      collection: this.collection
+      collection: this.collection,
+      granularity: 'minutes'
     });
   },
+
+  events: {
+    'change #granularity-selector': 'onTimelineUnitsChanged'
+  },
+
   /**
    * @property template
    * @type {Function}
    */
   template: require('./template'),
+
   /**
    * @method render
    * @returns {VocablistSideBar}
@@ -31,7 +40,25 @@ module.exports = GelatoComponent.extend({
     this._views['bargraph'].setElement('#time-studied-bar-graph-container').render();
   },
 
+  /**
+   * Runs entrance animations, fetches any data, and does any resetting that
+   * should be performed when this section of the stats page is made visible.
+   * @method onTabVisible
+   */
   onTabVisible: function() {
     this._views['bargraph'].redrawGraph();
+  },
+
+  /**
+   * Updates the granularity level of units(hours, minutes) in which to display
+   * certain graphs.
+   * @param {jQuery.Event} event the change event
+   * @method onTimelineUnitsChanged
+   */
+  onTimelineUnitsChanged: function(event) {
+    event.preventDefault();
+    var units = event.target.value;
+
+    this._views['bargraph'].updateUnits(units);
   }
 });
