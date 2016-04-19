50c9af25af25fd2c0dc4fb8663a4b1dd39322674
Michael Prokopchuk <prokopcm@gmail.com>
2016-04-19 18:23:21 -0400

    Add date range items-learned timeline graph, improve progress-stats date felxibility

--- app/components/stats/summary/view.js
+++ app/components/stats/summary/view.js
@@ -1,7 +1,7 @@
 var GelatoComponent = require('gelato/component');
-
-var StatsAllTimeComponent = require('components/stats/items-learned/view');
+var StatsItemsLearnedComponent = require('components/stats/items-learned/view');
 var StatsHeatmapComponent = require('components/stats/heatmap/view');
+
 /**
  * A component that is a composite of graphs which show user study statistics
  * for all time.
@@ -10,26 +10,28 @@ var StatsHeatmapComponent = require('components/stats/heatmap/view');
  */
 module.exports = GelatoComponent.extend({
   /**
-   *
+   * Initializes a new summary stats view
    * @method initialize
    * @constructor
    */
   initialize: function(options) {
     this._views = {};
-    this._views['allTime'] = new StatsAllTimeComponent({
-      collection: this.collection
+    this._views['allTime'] = new StatsItemsLearnedComponent({
+      collection: this.collection,
+      title: 'All Time'
     });
+
     this._views['heatmap'] = new StatsHeatmapComponent({
       collection: this.collection
     });
-
-    this.listenTo(this.collection, 'state:standby', this.updateGraphs);
   },
+
   /**
    * @property template
    * @type {Function}
    */
   template: require('./template'),
+
   /**
    * @method render
    * @returns {VocablistSideBar}
@@ -38,9 +40,5 @@ module.exports = GelatoComponent.extend({
     this.renderTemplate();
     this._views['allTime'].setElement('#stats-all-time-container').render();
     this._views['heatmap'].setElement('#stats-heatmap-container').render();
-  },
-
-  updateGraphs: function() {
-
   }
 });
