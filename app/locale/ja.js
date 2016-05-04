05e92b5f801e431c1e4c7b841088ef556a4f8d37
g1itch <jeremy@skritter.com>
2016-05-04 13:23:51 -0700

    Update ja.js

--- app/locale/ja.js
+++ app/locale/ja.js
@@ -7,7 +7,15 @@ module.exports = {
 
   "pages": {
     "contact": {
-      "title": "お問い合わせ"
+      "title": "お問い合わせ",
+	  "email": "Ｅメールアドレス",
+	  "topic": "話題",
+	  "message": "内容",
+	  "feedback": "フィードバック",
+	  "question": "質問",
+	  "bugReport": "バグレポート",
+	  "featureSuggestion": "新機能",
+	  "paymentIssue": "お支払い"
     },
     "home": {
       "freeTrial": "７日間無料トライアルを試してみよう！"
