cb98e75009b2a5409422d96f59262946f1e2b506
g1itch <jeremy@skritter.com>
2016-05-04 13:24:08 -0700

    Update en.js

--- app/locale/en.js
+++ app/locale/en.js
@@ -7,7 +7,15 @@ module.exports = {
 
   "pages": {
     "contact": {
-      "title": "Contact Us"
+      "title": "Contact Us",
+	  "email": "Email",
+	  "topic": "Topic",
+	  "message": "Message",
+	  "feedback": "Feedback",
+	  "question": "How to",
+	  "bugReport": "Bug Report",
+	  "featureSuggestion": "Feature Suggestion",
+	  "paymentIssue": "Payment Issue"
     },
     "home": {
       "freeTrial": "Start your 7-day free trial"
