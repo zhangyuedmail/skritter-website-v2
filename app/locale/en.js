8093045fd2ff98365acfa4be37d3e44a4088aeb7
g1itch <jeremy@skritter.com>
2016-05-05 11:19:05 -0700

    Update EN locale

--- app/locale/en.js
+++ app/locale/en.js
@@ -15,9 +15,18 @@ module.exports = {
 	  "question": "How to",
 	  "bugReport": "Bug Report",
 	  "featureSuggestion": "Feature Suggestion",
-	  "paymentIssue": "Payment Issue"
+	  "paymentIssue": "Payment Issue",
+	  "friendlyText": "Thanks for contacting us! We do respond to everything. If you're interested in Skritter for your school, click here.",
+	  "submit": "Submit"
     },
     "home": {
+	"motto1": "Chinese & Japanese",
+	  "motto2": "characters, made fun",
+	  "headerText": "The best way to learn Chinese and Japanese",
+	  "planeHeader": "Characters on the go",
+	  "planeText": "Learn Chinese & Japanese characters anywhere, even in Antarctica.",
+	  "flashcardsHeader": "Smart flashcards",
+	  "flashcardsText": "Skritter gives you immediate stroke-level feedback and helps you review hard characters more often.",
       "freeTrial": "Start your 7-day free trial"
     }
   }
