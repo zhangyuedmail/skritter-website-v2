dcd677798072334ce72a3ef98420be4db1494ac0
Michael Prokopchuk <prokopcm@gmail.com>
2016-06-26 15:34:35 -0500

    SKRIT-130: Add helper methods to the model to cleanup the template

--- app/models/vocablist-history.js
+++ app/models/vocablist-history.js
@@ -5,6 +5,22 @@ var SkritterModel = require('base/skritter-model');
  */
 module.exports = SkritterModel.extend({
   getChangedDate: function() {
-    return this.get('secondsAgo');
+    return moment().subtract(this.get('secondsAgo'), 'seconds').calendar();
+  },
+
+  getChanges: function() {
+    var d = this.get('deltas');
+    if (d.added.length) {
+      if (d.new_words && d.new_words.length) {
+        return 'Added ' + d.new_words.length + ' new words';
+      }
+      return 'Added ' + d.added[0];
+    }
+
+    if (d.removed.length) {
+      return 'Removed ' + d.removed[0];
+    }
+
+    return 'Made a change to the list.';
   }
 });
