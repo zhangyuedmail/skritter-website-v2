9987bb21d956d03677d56bb378f183b5001ad4f4
Joshua McFarland <mcfarljw@gmail.com>
2016-08-23 15:17:47 +0800

    Check for globally installed forever dependency when starting

--- scripts/npm-start.js
+++ scripts/npm-start.js
@@ -2,7 +2,13 @@
 
 const shell = require('shelljs');
 
+//require npm forever to be install globally
+if (!shell.exec('forever --version', {silent: true})) {
+  console.log('Install forever using "npm -g install forever"');
+  shell.exit();
+}
+
 process.env.DEVELOPMENT = false;
 shell.rm('-rf', './public');
-shell.exec('brunch build --production');
-shell.exec('node ./server.js');
+shell.exec('brunch build --production', {async: false});
+shell.exec('forever ./server.js', {async: false});
