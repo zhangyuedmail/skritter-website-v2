49c67de7c5dacdcd343c0ed30865508dbb4032b9
Joshua McFarland <mcfarljw@gmail.com>
2016-08-23 16:34:37 +0800

    Revert changes using forever with npm start script

--- scripts/npm-start.js
+++ scripts/npm-start.js
@@ -2,13 +2,6 @@
 
 const shell = require('shelljs');
 
-//require npm forever to be install globally
-if (!shell.exec('forever --version', {silent: true})) {
-  console.log('Install forever using "npm -g install forever"');
-  shell.exit();
-}
-
-process.env.DEVELOPMENT = false;
 shell.rm('-rf', './public');
 shell.exec('brunch build --production', {async: false});
-shell.exec('forever ./server.js', {async: false});
+shell.exec('node ./server.js', {async: false});
