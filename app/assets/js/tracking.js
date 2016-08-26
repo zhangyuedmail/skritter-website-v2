925376d12f84fc602e4be46c37a478f35d1ec85d
Joshua McFarland <mcfarljw@gmail.com>
2016-08-26 16:55:12 +0800

    SKRIT-11: Enable basic google and mixpanel tracking on static pages

--- app/assets/js/tracking.js
+++ app/assets/js/tracking.js
@@ -1,4 +1,4 @@
-// google analytics
+//google analytics
 (function(i, s, o, g, r, a, m) {
   i['GoogleAnalyticsObject'] = r;
   i[r] = i[r] || function() {
@@ -10,7 +10,8 @@
   a.src = g;
   m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-// mixpanel
+
+//mixpanel
 (function(e, b) {
   if (!b.__SV) {
     var a, f, i, g;
