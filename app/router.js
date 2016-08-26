56b62c73c2c549143b86912fe2eceabe9dc27035
Joshua McFarland <mcfarljw@gmail.com>
2016-08-26 17:26:33 +0800

    Remove internal routes for home and features

--- app/router.js
+++ app/router.js
@@ -33,8 +33,6 @@ module.exports = Router.extend({
     'dashboard': 'navigateDashboard',
     'demo': 'navigateDemo',
     'discourse/login': 'navigateDiscourseLogin',
-    'features': 'navigateFeatures',
-    'home': 'navigateHome',
     'institutions': 'navigateInstitutions',
     'legal': 'navigateLegal',
     'login': 'navigateLogin',
@@ -73,7 +71,7 @@ module.exports = Router.extend({
     if (app.user.isLoggedIn()) {
       this.navigateDashboard();
     } else {
-      this.navigateHome();
+      window.location.href = '/';
     }
   },
 
@@ -186,7 +184,7 @@ module.exports = Router.extend({
     }
   },
 
-   /**
+  /**
    * @method navigateAdmin
    */
   navigateAdmin: function() {
@@ -251,22 +249,6 @@ module.exports = Router.extend({
   navigateDiscourseLogin: function() {
     //TODO: redirect to legacy until support is added
     location.href = location.href.toString().replace('https://www.', 'http://legacy.');
-  },
-
-  /**
-   * @method navigateFeatures
-   */
-  navigateFeatures: function() {
-    this.navigate('features');
-    this.go('pages/features/FeaturesPage');
-  },
-
-  /**
-   * @method navigateHome
-   */
-  navigateHome: function() {
-    this.navigate('home');
-    this.go('pages/home/HomePage');
   },
 
   /**
@@ -550,4 +532,5 @@ module.exports = Router.extend({
       this.navigateLogin();
     }
   }
+
 });
