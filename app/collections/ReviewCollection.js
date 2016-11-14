e9be8d1f304e5d8123822240ed3bf730d8691e64
Joshua McFarland <mcfarljw@gmail.com>
2016-11-14 08:52:02 +0800

    SKRIT-312: Add additional measures for handling empty newInterval

--- app/collections/ReviewCollection.js
+++ app/collections/ReviewCollection.js
@@ -66,84 +66,91 @@ const ReviewCollection = BaseSkritterCollection.extend({
    * @param {Object} [options]
    */
   post: function(options) {
-    var self = this;
     options = _.defaults(options || {}, {async: true, skip: 0});
-    if (this.state === 'standby') {
-      this.state = 'posting';
-      this.trigger('state', this.state, this);
-      this.trigger('state:' + this.state, this);
-      var reviews = this.slice(0, -options.skip || this.length);
-      async.eachSeries(
-        _.chunk(reviews, 20),
-        function(chunk, callback) {
-          var data = _
-            .chain(chunk)
-            .map(function(review) {
-              return review.get('data');
-            })
-            .flatten()
-            .value();
-          //TODO: figure out why duplicates exist
-          data = _.uniqBy(data, function(review) {
-            return [
-              review.itemId,
-              review.currentInterval,
-              review.newInterval
-            ].join('');
-          });
-          $.ajax({
-            url: app.getApiUrl() + 'reviews',
-            async: options.async,
-            headers: app.user.session.getHeaders(),
-            type: 'POST',
-            data: JSON.stringify(data),
-            error: function(error) {
-              var items = _
-                .chain(error.responseJSON.errors)
-                .map('Item')
-                .without(undefined)
-                .value();
-              if (items.length) {
-                Raygun.send(
-                  new Error('Review Error: Items returned with errors'),
-                  {
-                    data: data,
-                    error: error.responseJSON
-                  }
-                );
-                self.reset();
-                callback(error);
-              } else {
-                Raygun.send(
-                  new Error('Review Error: Unable to post chunk'),
-                  {
-                    data: data,
-                    error: error.responseJSON
-                  }
-                );
-                self.remove(chunk);
-                callback();
-              }
 
-            },
-            success: function() {
-              self.remove(chunk);
-              setTimeout(callback, 1000);
-            }
-          });
-        },
-        function(error) {
-          self.state = 'standby';
-          self.trigger('state', self.state, self);
-          self.trigger('state:' + self.state, self);
-          if (error) {
-            console.error('REVIEW ERROR:', error);
-          } else {
-            console.log('REVIEWS POSTED:', reviews.length);
-          }
-        }
-      );
+    if (this.state !== 'standby') {
+      return;
     }
+
+    this.state = 'posting';
+    this.trigger('state', this.state, this);
+    this.trigger('state:' + this.state, this);
+
+    const reviews = this.slice(0, -options.skip || this.length);
+    async.eachSeries(
+      _.chunk(reviews, 20),
+      (chunk, callback) => {
+        const data = _
+          .chain(chunk)
+          .map(
+            function (review) {
+              return review.get('data');
+            }
+          )
+          .flatten()
+          .uniqBy(
+            function (review) {
+              return [review.itemId, review.currentInterval, review.newInterval].join('');
+            }
+          )
+          .value();
+
+        $.ajax({
+          url: app.getApiUrl() + 'reviews',
+          async: options.async,
+          headers: app.user.session.getHeaders(),
+          type: 'POST',
+          data: JSON.stringify(data),
+          error: (error) => {
+            const items = _
+              .chain(error.responseJSON.errors)
+              .map('Item')
+              .without(undefined)
+              .value();
+
+            if (items.length) {
+              Raygun.send(
+                new Error('Review Error: Items returned with errors'),
+                {
+                  data: data,
+                  error: error.responseJSON
+                }
+              );
+
+              this.reset();
+
+              callback(error);
+            } else {
+              Raygun.send(
+                new Error('Review Error: Unable to post chunk'),
+                {
+                  data: data,
+                  error: error.responseJSON
+                }
+              );
+
+              this.remove(chunk);
+
+              callback();
+            }
+          },
+          success: () => {
+            this.remove(chunk);
+            setTimeout(callback, 1000);
+          }
+        });
+      },
+      (error) => {
+        this.state = 'standby';
+        this.trigger('state', self.state, self);
+        this.trigger('state:' + self.state, self);
+        if (error) {
+          console.error('REVIEW ERROR:', error);
+        } else {
+          console.log('REVIEWS POSTED:', reviews.length);
+        }
+      }
+    );
   },
 
   /**
@@ -153,31 +160,43 @@ const ReviewCollection = BaseSkritterCollection.extend({
    * @returns {Array}
    */
   put: function(models, options) {
-    var updatedReviews = [];
+    const updatedReviews = [];
 
     models = _.isArray(models) ? models : [models];
     options = _.defaults(options || {}, {merge: true});
 
-    for (var a = 0, lengthA = models.length; a < lengthA; a++) {
-      var model = models[a];
+    for (let a = 0, lengthA = models.length; a < lengthA; a++) {
+      const model = models[a];
+
       model.data = _.uniqBy(model.data, 'itemId');
+
       for (var b = 0, lengthB = model.data.length; b < lengthB; b++) {
-        var modelData = model.data[b];
-        var item = modelData.item.clone();
-        var submitTimeSeconds = Math.round(modelData.submitTime);
+        const modelData = model.data[b];
+        const item = modelData.item.clone();
+        const submitTimeSeconds = Math.round(modelData.submitTime);
 
         modelData.score = parseInt(modelData.score, 10);
 
-        if (modelData.score) {
-          modelData.newInterval = app.fn.interval.quantify(item.toJSON(), modelData.score);
-        } else {
+        if (_.isEmpty(modelData.score)) {
           modelData.score = 3;
-          modelData.newInterval = 86400;
         }
 
         modelData.actualInterval = item.get('last') ? submitTimeSeconds - item.get('last') : 0;
+        modelData.newInterval = app.fn.interval.quantify(item.toJSON(), modelData.score);
         modelData.previousInterval = item.get('previousInterval') || 0;
         modelData.previousSuccess = item.get('previousSuccess') || false;
+
+        if (_.isEmpty(modelData.newInterval)) {
+          Raygun.send(
+            new Error('Review Error: Missing new interval value'),
+            {
+              data: modelData,
+              models: models
+            }
+          );
+
+          modelData.newInterval = 86400;
+        }
 
         if (app.isDevelopment()) {
           console.log(
@@ -196,6 +215,7 @@ const ReviewCollection = BaseSkritterCollection.extend({
 
     return updatedReviews;
   }
+
 });
 
 module.exports = ReviewCollection;
