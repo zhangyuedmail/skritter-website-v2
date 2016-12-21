aea64e9bf8fd87c4e3fec7eccc1b8cc871a06760
Joshua McFarland <mcfarljw@gmail.com>
2016-12-20 20:45:54 -0500

    Update android release script

--- scripts/npm-android-release.js
+++ scripts/npm-android-release.js
@@ -13,17 +13,13 @@ shell.exec('brunch build');
 shell.cp('./cordova.xml', './cordova/config.xml');
 
 if (lang === 'ja') {
-  shell.exec('cordova plugin add cc.fovea.cordova.purchase --variable BILLING_KEY=' + billing.skritter_chinese);
   shell.sed('-i', '{!application-id!}', 'com.inkren.skritter.japanese', './cordova/config.xml');
   shell.sed('-i', '{!application-language!}', 'japanese', './cordova/config.xml');
   shell.sed('-i', '{!application-name!}', 'Skritter Japanese', './cordova/config.xml');
-  shell.cp('./skritter-japanese.build.json', './cordova/build.json');
 } else {
-  shell.exec('cordova plugin add cc.fovea.cordova.purchase --variable BILLING_KEY=' + billing.skritter_japanese);
   shell.sed('-i', '{!application-id!}', 'com.inkren.skritter.chinese', './cordova/config.xml');
   shell.sed('-i', '{!application-language!}', 'chinese', './cordova/config.xml');
   shell.sed('-i', '{!application-name!}', 'Skritter Chinese', './cordova/config.xml');
-  shell.cp('./skritter-chinese.build.json', './cordova/build.json');
 }
 
 shell.sed('-i', '{!application-version!}', project.version, './cordova/config.xml');
@@ -32,6 +28,16 @@ shell.rm('-rf', './cordova/www/*');
 shell.cp('-r', './public/*', './cordova/www');
 
 shell.cd('./cordova');
+
+if (lang === 'ja') {
+  shell.exec('cordova plugin add cc.fovea.cordova.purchase --variable BILLING_KEY=' + billing.skritter_chinese);
+  shell.cp('../skritter-japanese.build.json', './build.json');
+
+} else {
+  shell.exec('cordova plugin add cc.fovea.cordova.purchase --variable BILLING_KEY=' + billing.skritter_japanese);
+  shell.cp('../skritter-chinese.build.json', './build.json');
+}
+
 shell.exec('cordova build android --release --buildConfig');
 
 if (lang === 'ja') {
