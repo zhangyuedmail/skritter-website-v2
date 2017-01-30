e04d12aa9c153ace07a2e0216fccb9d54313c7fa
Joshua McFarland <mcfarljw@gmail.com>
2017-01-30 09:50:14 +0800

    Update core plugin to include support for hanping and pleco buttons

--- plugins/core/www/core.js
+++ plugins/core/www/core.js
@@ -1,10 +1,32 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-    openGooglePlay: function(packageName) {
-        exec(null, null, 'SkritterPlugin', 'openGooglePlay', [packageName]);
-    },
-    openPleco: function(text) {
-        exec(null, null, 'SkritterPlugin', 'openPleco', [text]);
-    }
+  isPackageInstalled: function(packageName, callback) {
+    exec(
+      function() {
+        typeof callback === 'function' && callback(true);
+      },
+      function() {
+        typeof callback === 'function' && callback(false);
+      },
+      'SkritterPlugin',
+      'isPackageInstalled',
+      [packageName]
+    );
+  },
+  openGooglePlay: function(packageName) {
+    exec(null, null, 'SkritterPlugin', 'openGooglePlay', [packageName]);
+  },
+  openHanpingLite: function(text) {
+    exec(null, null, 'SkritterPlugin', 'openHanpingLite', [text]);
+  },
+  openHanpingPro: function(text) {
+    exec(null, null, 'SkritterPlugin', 'openHanpingPro', [text]);
+  },
+  openHanpingYue: function(text) {
+    exec(null, null, 'SkritterPlugin', 'openHanpingYue', [text]);
+  },
+  openPleco: function(text) {
+    exec(null, null, 'SkritterPlugin', 'openPleco', [text]);
+  }
 };
