e17837fe7436fea34793194230ffb487038d0dda
Joshua McFarland <mcfarljw@gmail.com>
2017-06-06 08:42:30 +0800

    Commit a comment for getting better brunch logs via loggy_stacks

--- scripts/npm-monitor.js
+++ scripts/npm-monitor.js
@@ -5,6 +5,9 @@ const shell = require('shelljs');
 // for whether to use local backend
 process.env.THINK_LOCALLY = process.argv.length > 2  && process.argv[2] === 'thinkLocally';
 
+// use this for better brunch logs
+// process.env.LOGGY_STACKS = true;
+
 process.env.NODE_ENV = 'development';
 shell.rm('-rf', './public');
 shell.exec('brunch watch', {async: true});
