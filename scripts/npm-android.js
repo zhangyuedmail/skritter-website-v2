6df484a9b99e80b950aca22549ae42b44f5a006f
Joshua McFarland <mcfarljw@gmail.com>
2017-06-07 13:21:04 +0800

    Use versionCode from package.json when building android

--- scripts/npm-android.js
+++ scripts/npm-android.js
@@ -27,6 +27,8 @@ shell.cp('-r', './public/*', './cordova/www');
 shell.cp('-f', './res/gradle.properties', './cordova/platforms/android/gradle.properties');
 shell.cp('-f', './res/xwalk-command-line', './cordova/platforms/android/assets/xwalk-command-line');
 
+shell.sed('-i', '{!application-versionCode!}', project.versionCode, './cordova/platforms/android/gradle.properties');
+
 shell.cd('./cordova');
 
 if (lang === 'ja') {
