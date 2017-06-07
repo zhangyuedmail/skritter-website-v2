3ba94e3ce7ffd0c352735de214cfd3d60ee1f5cf
Joshua McFarland <mcfarljw@gmail.com>
2017-06-07 13:13:14 +0800

    Specify cordova plugin version numbers for build stability

--- scripts/npm-install-cordova.js
+++ scripts/npm-install-cordova.js
@@ -11,13 +11,13 @@ shell.exec('cordova platform add android@6.2.3');
 shell.exec('cordova platform add ios@4.4.0');
 
 // plugins
-shell.exec('cordova plugin add cordova-plugin-crosswalk-webview');
-shell.exec('cordova plugin add cordova-plugin-device');
-shell.exec('cordova plugin add cordova-plugin-file');
-shell.exec('cordova plugin add cordova-plugin-file-transfer');
-shell.exec('cordova plugin add cordova-plugin-media');
-shell.exec('cordova plugin add cordova-plugin-splashscreen');
-shell.exec('cordova plugin add cordova-plugin-statusbar');
+shell.exec('cordova plugin add cordova-plugin-crosswalk-webview@2.3.0');
+shell.exec('cordova plugin add cordova-plugin-device@1.1.6');
+shell.exec('cordova plugin add cordova-plugin-file@4.3.3');
+shell.exec('cordova plugin add cordova-plugin-file-transfer@1.6.3');
+shell.exec('cordova plugin add cordova-plugin-media@3.0.1');
+shell.exec('cordova plugin add cordova-plugin-splashscreen@4.0.3');
+shell.exec('cordova plugin add cordova-plugin-statusbar@2.2.3');
 shell.exec('cordova plugin add https://github.com/phonegap/phonegap-mobile-accessibility.git');
 shell.exec('cordova plugin add https://github.com/mcfarljw/cordova-plugin-billing.git');
 shell.exec('cordova plugin add ../plugins/core');
