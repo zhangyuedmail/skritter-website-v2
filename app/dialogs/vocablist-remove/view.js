f8c69ba12c1184e47d0129880474c1dd5b29f30a
Michael Prokopchuk <prokopcm@gmail.com>
2017-06-09 18:06:50 -0500

    SKRIT-501: Add functionality to properly remove associated vocabs when a user removes a vocablist

--- app/dialogs/vocablist-remove/view.js
+++ app/dialogs/vocablist-remove/view.js
@@ -26,6 +26,12 @@ module.exports = BootstrapDialog.extend({
    */
   initialize: function(options) {
     this.vocablist = options.vocablist;
+    this.vocablistId = this.vocablist.id;
+
+    this.itemsCursor = '';
+    this.itemsRemoved = 0;
+    this.itemsToRemove = 0;
+    this.toRemove = ["item", "sect_complete"];
 
     if (!this.vocablist) {
       throw new Error('VocablistRemoveDialog requires a vocablist passed in!')
@@ -40,6 +46,56 @@ module.exports = BootstrapDialog.extend({
     this.renderTemplate();
 
     return this;
+  },
+
+  /**
+   * Gets a count of all the vocab in the list, then deletes them
+   */
+  deleteVocabFromList: function() {
+
+    this.getListVocabCount().then(() => {
+
+      // TODO: update UI with count
+      this._removeVocab().then(() => {
+        this.close();
+      }, (error) => {
+        this.showError('Error deleting associated vocab from list. Please contact the Skritter team regarding list ' + this.vocablistId);
+      });
+    }, (error) => {
+      this.showError('Error deleting associated vocab from list. Please contact the Skritter team regarding list ' + this.vocablistId);
+    });
+  },
+
+  /**
+   * Gets the total count
+   * @return {Promise}
+   */
+  getListVocabCount: function() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        context: this,
+        url: app.getApiUrl() + 'vocablists/' + this.vocablistId + '/vocab-count?cursor=' + this.itemsCursor,
+        type: 'GET',
+        headers: app.user.session.getHeaders(),
+        error: function(error) {
+          reject();
+        },
+        success: (resp) => {
+          this.itemsToRemove += resp.num;
+
+          if (resp.cursor) {
+            this.itemsCursor = resp.cursor;
+            this.getListVocabCount().then(() => {
+              resolve();
+            }, (error) => {
+              reject();
+            });
+          } else {
+            resolve();
+          }
+        }
+      });
+    });
   },
 
   /**
@@ -60,7 +116,12 @@ module.exports = BootstrapDialog.extend({
       method: 'PUT',
       success: () => {
         this.hideError();
-        this.close();
+
+        if (app.isDevelopment()) {
+          this.deleteVocabFromList();
+        } else {
+          this.close();
+        }
       },
       error: (error) => {
         this.resetUI();
@@ -89,6 +150,14 @@ module.exports = BootstrapDialog.extend({
   },
 
   /**
+   * Shows an update message to the user on the progress of deleting the vocab associations from the list
+   * @param {String} msg the error message to show
+   */
+  showVocabDeletionStatusText: function(msg) {
+    this.$('#deleting-status').text(msg).removeClass('hidden');
+  },
+
+  /**
    * Resets the dialog to its initial state and hides any error messages
    */
   resetUI: function() {
@@ -96,5 +165,74 @@ module.exports = BootstrapDialog.extend({
     this.$('.modal-footer').removeClass('hidden');
     this.$('.step-1').removeClass('hidden');
     this.$('.step-2').addClass('hidden');
+  },
+
+  /**
+   * Generates a status text update on the progress of the vocab deletion
+   */
+  updateVocabDeletionStatusText: function() {
+    let percentDone = 100 * this.itemsRemoved / Math.max(this.itemsToRemove, 1);
+    percentDone = parseInt(Math.min(99, percentDone), 10);
+
+    let toGo = this.itemsToRemove - this.itemsRemoved;
+    let statusText = percentDone + "% complete ";
+    if (toGo > 0) {
+      statusText += "(" + to_go + " to process)";
+    }
+
+    if (this.toRemove[0] !== 'item') {
+      statusText = 'WrappingUp';
+    }
+    this.showVocabDeletionStatusText(statusText);
+  },
+
+  /**
+   * Removes associated vocab from the user's study queue in batches.
+   */
+  _removeVocab: function() {
+    this.updateVocabDeletionStatusText();
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        context: this,
+        url: app.getApiUrl() + 'vocablists/' + this.vocablistId + '/vocab',
+        type: 'DELETE',
+        headers: app.user.session.getHeaders(),
+        data: {
+          kind: this.toRemove[0],
+          offset: this.itemsRemoved
+        },
+        error: function(error) {
+          reject();
+        },
+        success: (resp) => {
+          this.itemsRemoved += resp.num;
+
+          if (resp.more) {
+            this._removeVocab().then(() => {
+              resolve();
+            }, (error) => {
+              reject();
+            });
+          } else {
+            this.toRemove.shift();
+
+            // means we still have sect_complete to update
+            if (this.toRemove.length) {
+              this._removeVocab().then(() => {
+                resolve();
+              }, (error) => {
+                reject();
+              });
+            }
+
+            // means we've updated both items and sect_complete, we're done
+            else {
+              resolve();
+            }
+          }
+        }
+      });
+    });
   }
 });
