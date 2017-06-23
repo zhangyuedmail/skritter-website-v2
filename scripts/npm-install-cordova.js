6b8a342c8b38abaebc91b00f5aa7a2733345cc6e
Joshua McFarland <mcfarljw@gmail.com>
2017-06-23 17:45:17 +0800

    Finally add some ios build script fragments with temporary hack

--- scripts/npm-install-cordova.js
+++ scripts/npm-install-cordova.js
@@ -21,3 +21,8 @@ shell.exec('cordova plugin add cordova-plugin-statusbar@2.2.3');
 shell.exec('cordova plugin add https://github.com/phonegap/phonegap-mobile-accessibility.git');
 shell.exec('cordova plugin add https://github.com/mcfarljw/cordova-plugin-billing.git');
 shell.exec('cordova plugin add ../plugins/core');
+
+// TODO: remove once issue is fixed
+// https://github.com/phonegap/ios-sim/issues/210
+shell.cd('./platforms/ios/cordova');
+shell.exec('npm install ios-sim@latest');
