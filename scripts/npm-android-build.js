b09d66fedc6fd7330d5b82a5f1bc9cd5dfb69da5
Joshua McFarland <mcfarljw@gmail.com>
2017-08-08 15:00:51 +0800

    Add ios build script and package.json bundle version information

---
+++ scripts/npm-android-build.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const lang = process.argv[2] || 'zh';
+const project = require('../package.json');
+const shell = require('shelljs');
+
+process.env.PROJECT_LANG = lang;
+
+shell.exec('brunch build');
+
+shell.cp('./cordova.xml', './cordova/config.xml');
+
+if (lang === 'ja') {
+  shell.sed('-i', '{!application-id!}', 'com.inkren.skritter.japanese', './cordova/config.xml');
+  shell.sed('-i', '{!application-language!}', 'japanese', './cordova/config.xml');
+  shell.sed('-i', '{!application-name!}', 'Skritter Japanese', './cordova/config.xml');
+} else {
+  shell.sed('-i', '{!application-id!}', 'com.inkren.skritter.chinese', './cordova/config.xml');
+  shell.sed('-i', '{!application-language!}', 'chinese', './cordova/config.xml');
+  shell.sed('-i', '{!application-name!}', 'Skritter Chinese', './cordova/config.xml');
+}
+
+shell.sed('-i', '{!application-version!}', project.version, './cordova/config.xml');
+
+shell.rm('-rf', './cordova/www/*');
+shell.cp('-r', './public/*', './cordova/www');
+shell.cp('-f', './res/gradle.properties', './cordova/platforms/android/gradle.properties');
+shell.cp('-f', './res/xwalk-command-line', './cordova/platforms/android/assets/xwalk-command-line');
+
+shell.sed('-i', '{!application-versionCode!}', project.versionCode, './cordova/platforms/android/gradle.properties');
+
+shell.cd('./cordova');
+
+if (lang === 'ja') {
+  shell.cp('../skritter-japanese.build.json', './build.json');
+} else {
+  shell.cp('../skritter-chinese.build.json', './build.json');
+}
+
+shell.exec('cordova build android --buildConfig');
---
+++ scripts/npm-ios-build.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const lang = process.argv[2] || 'zh';
+const project = require('../package.json');
+const shell = require('shelljs');
+
+process.env.PROJECT_LANG = lang;
+
+shell.exec('brunch build');
+
+shell.cp('./cordova.xml', './cordova/config.xml');
+
+if (lang === 'ja') {
+  shell.sed('-i', '{!application-id!}', 'com.skritter.skritterja', './cordova/config.xml');
+  shell.sed('-i', '{!application-language!}', 'japanese', './cordova/config.xml');
+  shell.sed('-i', '{!application-name!}', 'Skritter Japanese', './cordova/config.xml');
+} else {
+  shell.sed('-i', '{!application-id!}', 'com.skritter.skritter', './cordova/config.xml');
+  shell.sed('-i', '{!application-language!}', 'chinese', './cordova/config.xml');
+  shell.sed('-i', '{!application-name!}', 'Skritter Chinese', './cordova/config.xml');
+}
+
+shell.sed('-i', '{!application-version!}', project.iosVersion, './cordova/config.xml');
+shell.sed('-i', '{!application-bundleVersion!}', project.iosVersionBundle, './cordova/config.xml');
+
+shell.rm('-rf', './cordova/www/*');
+shell.cp('-r', './public/*', './cordova/www');
+
+shell.cd('./cordova');
+
+if (lang === 'ja') {
+  shell.cp('../skritter-japanese.build.json', './build.json');
+} else {
+  shell.cp('../skritter-chinese.build.json', './build.json');
+}
+
+shell.exec('cordova platform rm ios');
+shell.exec('cordova platform add ios');
+shell.exec('cordova build ios --buildConfig');
