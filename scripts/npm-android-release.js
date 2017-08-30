6b2d16fa7f9d9f6f34d2cf80c7703543a70d15e1
Joshua McFarland <mcfarljw@gmail.com>
2017-08-30 11:43:52 +1000

    Adjust icon naming to just "Skritter" for both Chinese and Japanese on Android

--- scripts/npm-android-release.js
+++ scripts/npm-android-release.js
@@ -14,11 +14,11 @@ shell.cp('./cordova.xml', './cordova/config.xml');
 if (lang === 'ja') {
   shell.sed('-i', '{!application-id!}', 'com.inkren.skritter.japanese', './cordova/config.xml');
   shell.sed('-i', '{!application-language!}', 'japanese', './cordova/config.xml');
-  shell.sed('-i', '{!application-name!}', 'Skritter Japanese', './cordova/config.xml');
+  shell.sed('-i', '{!application-name!}', 'Skritter', './cordova/config.xml');
 } else {
   shell.sed('-i', '{!application-id!}', 'com.inkren.skritter.chinese', './cordova/config.xml');
   shell.sed('-i', '{!application-language!}', 'chinese', './cordova/config.xml');
-  shell.sed('-i', '{!application-name!}', 'Skritter Chinese', './cordova/config.xml');
+  shell.sed('-i', '{!application-name!}', 'Skritter', './cordova/config.xml');
 }
 
 shell.sed('-i', '{!application-version!}', project.version, './cordova/config.xml');
