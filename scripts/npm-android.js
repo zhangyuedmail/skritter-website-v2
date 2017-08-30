0890789dcdb113b3e9253290cc89ebfa4d73d5bb
Joshua McFarland <mcfarljw@gmail.com>
2017-08-30 11:39:09 +1000

    Install debug chinese builds using test id

--- scripts/npm-android.js
+++ scripts/npm-android.js
@@ -15,7 +15,7 @@ if (lang === 'ja') {
   shell.sed('-i', '{!application-language!}', 'japanese', './cordova/config.xml');
   shell.sed('-i', '{!application-name!}', 'Skritter Test', './cordova/config.xml');
 } else {
-  shell.sed('-i', '{!application-id!}', 'com.inkren.skritter.chinese', './cordova/config.xml');
+  shell.sed('-i', '{!application-id!}', 'com.inkren.skritter.chinese.test', './cordova/config.xml');
   shell.sed('-i', '{!application-language!}', 'chinese', './cordova/config.xml');
   shell.sed('-i', '{!application-name!}', 'Skritter Test', './cordova/config.xml');
 }
