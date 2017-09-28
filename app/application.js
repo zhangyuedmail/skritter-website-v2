013388417dacc794e694edf50c5ae301f0295704
Michael Prokopchuk <prokopcm@gmail.com>
2017-09-28 15:20:30 -0500

    Add vertical offset to user notifications to improve mobile experience

--- app/application.js
+++ app/application.js
@@ -643,6 +643,10 @@ module.exports = GelatoApplication.extend({
         message: options.message
       },
       {
+        offset: {
+          x: 20,
+          y: app.isMobile() ? 100 : 20
+        },
         type: options.type || 'pastel-danger',
         animate: {
           enter: options.animateEnter || 'animated fadeInDown',
