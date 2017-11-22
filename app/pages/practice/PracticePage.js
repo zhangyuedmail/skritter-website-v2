40025f3941301c878f111cc6be127cfd149297d2
Michael Prokopchuk <prokopcm@gmail.com>
2017-11-22 16:15:31 -0500

    SKRIT-642: Refactor fetching code

--- app/pages/practice/PracticePage.js
+++ app/pages/practice/PracticePage.js
@@ -5,8 +5,6 @@ const ItemCollection = require('collections/ItemCollection');
 const VocabCollection = require('collections/VocabCollection');
 const QuickSettings = require('dialogs1/quick-settings/QuickSettingsDialog.js');
 
-const vent = require('vent');
-
 /**
  * @class PracticePadPage
  * @extends {GelatoPage}
@@ -41,6 +39,7 @@ const PracticePadPage = GelatoPage.extend({
       this.loadAlreadyTimed = false;
     }
 
+    _.bindAll(this, '_onInitialVocabDataLoaded');
     this.targetLang = options.targetLang;
 
     // TODO: send this in from URL
@@ -62,12 +61,21 @@ const PracticePadPage = GelatoPage.extend({
     this.items = new ItemCollection();
     this.vocabs = new VocabCollection();
 
-    this._views['prompt'] = new Prompt({page: this});
-    this._views['toolbar'] = new Toolbar({page: this});
+    this._views['prompt'] = new Prompt({
+      page: this,
+      vocabToolbarButtonState: {
+        showAudio: true,
+        showBan: false,
+        showInfo: false,
+        showStar: false,
+      },
+    });
+    this._views['toolbar'] = new Toolbar({
+      page: this,
+    });
 
     this.listenTo(this.prompt, 'next', this.handlePromptNext);
     this.listenTo(this.prompt, 'previous', this.handlePromptPrevious);
-    this.listenTo(vent, 'studySettings:show', this.showStudySettings);
 
     // handle specific cordova related events
     document.addEventListener('pause', this.handlePauseEvent.bind(this), false);
@@ -86,91 +94,23 @@ const PracticePadPage = GelatoPage.extend({
     this._views['prompt'].setElement('#study-prompt-container').render();
     this._views['toolbar'].setElement('#study-toolbar-container').render();
 
-    this.checkRequirements();
+    this.loadInitialVocab();
 
     return this;
   },
 
   /**
-   * @method checkRequirements
+   * @method loadInitialVocab
    */
-  checkRequirements () {
+  loadInitialVocab () {
     ScreenLoader.post('Preparing practice');
     const self = this;
 
-    async.waterfall(
-      [(callback) => {
-        this.vocabs.fetch({
-          data: {
-            include_decomps: true,
-            include_heisigs: true,
-            include_sentences: false,
-            include_top_mnemonics: true,
-            ids: this.idsToLoad,
-          },
-          error: function (error) {
-            callback(error);
-          },
-          success: function (vocabs) {
-            self.vocab = vocabs.at(0);
-            callback();
-          },
-        });
-      },
-      function(callback) {
-        if (self.vocab.has('containedVocabIds')) {
-          self.vocabs.fetch({
-            data: {
-              include_decomps: true,
-              include_sentences: true,
-              ids: self.vocab.get('containedVocabIds').join('|'),
-            },
-            remove: false,
-            error: function(error) {
-              callback(error);
-            },
-            success: function() {
-              callback();
-            },
-          });
-        } else {
-          callback();
-        }
-      },
-      function (callback) {
-        app.user.characters.fetch({
-          data: {
-            languageCode: self.vocab.get('lang'),
-            writings: self.vocab.get('writing'),
-          },
-          remove: false,
-          error: function (error) {
-            callback(error);
-          },
-          success: function () {
-            callback();
-          },
-        });
-      },
-      ],
-      (error) => {
-        if (error) {
-          let msg = 'Error fetching items. Please ';
-          if (app.isMobile()) {
-            msg += 'reload the application';
-          } else {
-            msg += 'refresh the page.';
-          }
-          ScreenLoader.post(msg);
-          return;
-        }
-
-        const runeItems = self.vocab.getPromptItems('rune');
-        self.promptItems = runeItems;
-        this._views['prompt'].set(this.promptItems);
-        ScreenLoader.hide();
-      }
-    );
+    this._fetchListOfVocabs(this.idsToLoad).then(() => {
+      this._fetchContainedVocab((self.vocab.get('containedVocabIds') || []).join('|')).then(() => {
+        this._fetchCharacters(self.vocab.get('lang'), self.vocab.get('writing')).then(this._onInitialVocabDataLoaded, this._onLoadError);
+      }, this._onLoadError);
+    }, this._onLoadError);
   },
 
   /**
@@ -332,6 +272,109 @@ const PracticePadPage = GelatoPage.extend({
    */
   togglePromptLoading (loading) {
     this._views['prompt'].toggleLoadingIndicator(loading);
+  },
+
+  /**
+   * Loads character data for a set of runes in a target language
+   * @param {String} languageCode 2-character code for the target language
+   * @param {String} writings character runes to load
+   */
+  _fetchCharacters (languageCode, writings) {
+    return new Promise((resolve, reject) => {
+      app.user.characters.fetch({
+        data: {
+          languageCode,
+          writings,
+        },
+        remove: false,
+        error: function (error) {
+          reject(error);
+        },
+        success: function () {
+          resolve();
+        },
+      });
+    });
+  },
+
+  /**
+   * Fetches any contained vocab for a character
+   * @param {String} ids contained character ids to load
+   */
+  _fetchContainedVocab (ids) {
+    return new Promise((resolve, reject) => {
+      if (this.vocab.has('containedVocabIds')) {
+        this.vocabs.fetch({
+          data: {
+            include_decomps: true,
+            include_sentences: true,
+            ids,
+          },
+          remove: false,
+          error: function (error) {
+            reject(error);
+          },
+          success: function () {
+            resolve();
+          },
+        });
+      } else {
+        resolve();
+      }
+    });
+  },
+
+  /**
+   * Fetches a list of vocabs from a specified string of ids
+   * @param {String} list a separated string of vocabs to fetch
+   */
+  _fetchListOfVocabs (list) {
+    return new Promise((resolve, reject) => {
+      this.vocabs.fetch({
+        data: {
+          include_decomps: true,
+          include_heisigs: true,
+          include_sentences: false,
+          include_top_mnemonics: true,
+          ids: this.idsToLoad,
+        },
+        error: (error) => {
+          reject(error);
+        },
+        success: (vocabs) => {
+          this.vocab = vocabs.at(0);
+          resolve();
+        },
+      });
+    });
+  },
+
+  /**
+   * Handles a successful load of vocab data. Sets up the prompt and
+   * hides the screenloader.
+   */
+  _onInitialVocabDataLoaded () {
+    const runeItems = this.vocab.getPromptItems('rune');
+    this.promptItems = runeItems;
+    this._views['prompt'].set(this.promptItems);
+    ScreenLoader.hide();
+  },
+
+  /**
+   * Shows a loading error to the user on the ScreenLoader
+   */
+  _onLoadError (error) {
+    let msg = 'Error fetching items. Please ';
+    if (app.isMobile()) {
+      msg += 'reload the application';
+    } else {
+      msg += 'refresh the page.';
+    }
+    ScreenLoader.post(msg);
+
+    if (app.isDevelopment() && error) {
+      console.error(error);
+    }
   },
 
   /**
