e599048e4326e027ca2528ffba6a4744b71f68f4
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2017-12-08 14:13:25 +0800

    Redirect study section routes to list until working

--- app/router.js
+++ app/router.js
@@ -393,7 +393,9 @@ module.exports = Router.extend({
   navigateStudy: function (listId, sectionId) {
     if (app.user.isLoggedIn()) {
       if (listId && sectionId) {
-        this.go('pages/study-list-section/StudyListSectionPage.js', {listId, sectionId});
+        // TODO: enable section study page when supported
+        // this.go('pages/study-list-section/StudyListSectionPage.js', {listId, sectionId});
+        this.go('pages/study-list/StudyListPage.js', {listId});
       } else if (listId) {
         this.go('pages/study-list/StudyListPage.js', {listId});
       } else {
