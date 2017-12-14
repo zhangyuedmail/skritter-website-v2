36c342fefa4f0455908d2f8b4ae089b73f18970e
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2017-12-14 18:08:51 +0800

    SKRIT-673: Allow for saving addFrequency value of 0 from study settings

--- app/pages/account/AccountSettingsStudyPage.js
+++ app/pages/account/AccountSettingsStudyPage.js
@@ -163,8 +163,10 @@ const AccountSettingsStudyPage = GelatoPage.extend({
       dailyAddLimit = 0;
     }
 
+    console.log(this.$('#field-add-frequency').val());
+
     app.user.set({
-      addFrequency: parseInt(this.$('#field-add-frequency').val(), 10) || app.user.get('addFrequency'),
+      addFrequency: parseInt(this.$('#field-add-frequency').val(), 10),
       autoAddComponentCharacters: this.$('#field-add-contained').is(':checked'),
       autoAdvancePrompts: this.$('#field-auto-advance').is(':checked') ? 1.0 : 0,
       dailyAddLimit,
